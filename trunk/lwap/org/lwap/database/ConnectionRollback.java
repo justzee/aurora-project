7927d128bed24339ff56a2840b3be8c2388d4cb5
seacat.zhou@gmail.com <seacat.zhou@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2009-06-21 08:37:52 +0000

    support multiple data source

--- trunk/lwap/org/lwap/database/ConnectionRollback.java
+++ trunk/lwap/org/lwap/database/ConnectionRollback.java
@@ -5,6 +5,9 @@ package org.lwap.database;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.lwap.controller.MainService;
 
@@ -12,15 +15,32 @@ import uncertain.proc.IExceptionHandle;
 import uncertain.proc.ProcedureRunner;
 
 public class ConnectionRollback implements IExceptionHandle {
+    
+    public static void rollback( Connection conn ){
+        try {
+            //System.out.println("rolling back");
+            conn.rollback();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }        
+    }
 
     public boolean handleException(ProcedureRunner runner, Throwable exception) {
+        // rollback connections
         Connection conn = (Connection)runner.getContext().get(MainService.KEY_CURRENT_CONNECTION);        
         if (conn != null) {
-            try {
-                //System.out.println("rolling back");
-                conn.rollback();
-            } catch (SQLException ex) {
-                ex.printStackTrace();
+            rollback(conn);
+        }
+        // rollback connection set
+        Map conn_map = (Map)runner.getContext().get(MainService.KEY_CONNECTION_SET);
+        if(conn_map!=null){
+            Collection c = conn_map.values();
+            if(c!=null){
+                Iterator it = c.iterator();
+                while(it.hasNext()){
+                    Connection conn1 = (Connection)it.next();
+                    rollback(conn1);
+                }
             }
         }
         return false;
