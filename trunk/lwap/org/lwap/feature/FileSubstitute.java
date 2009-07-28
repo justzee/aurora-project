fd39efd18d5632342c9b0ce1fb8dff29137e635e
seacat.zhou@gmail.com <seacat.zhou@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2009-07-28 03:54:15 +0000

    Add FileSubstitute feature

---
+++ trunk/lwap/org/lwap/feature/FileSubstitute.java
@@ -0,0 +1,34 @@
+/*
+ * Created on 2009-7-28
+ */
+package org.lwap.feature;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import uncertain.composite.CompositeMap;
+import uncertain.composite.CompositeUtil;
+import uncertain.core.IGlobalInstance;
+
+public class FileSubstitute implements IFileSubstitute, IGlobalInstance {
+    
+    Map             mFileMap = new HashMap();
+    CompositeMap    mFiles;
+
+    public String getRealFile(String requested_file) {
+        // TODO Auto-generated method stub
+        String new_file = (String)mFileMap.get(requested_file);
+        return new_file == null? requested_file: new_file;
+    }
+    
+    public void addFiles( CompositeMap files ){
+        this.mFiles = files;
+        CompositeUtil.fillMap(mFileMap, mFiles, "origin_file", "new_file");
+        System.out.println(mFileMap.toString());
+    }
+    
+    public CompositeMap getFiles(){
+        return mFiles;
+    }
+
+}
