f27874bbe104b25cf4cd02025c7b479ec470ccb2
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2009-10-25 15:26:07 +0000

    根文件

--- trunk/uncertain_ide/uncertain/ide/Activator.java
+++ trunk/uncertain_ide/uncertain/ide/Activator.java
@@ -1,8 +1,33 @@
 package uncertain.ide;
 
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+
+import uncertain.composite.CompositeMap;
+import uncertain.composite.QualifiedName;
+import uncertain.schema.Element;
+import uncertain.schema.Schema;
+import uncertain.schema.SchemaConstant;
+import uncertain.schema.SchemaManager;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -15,6 +40,7 @@ public class Activator extends AbstractUIPlugin {
 	// The shared instance
 	private static Activator plugin;
 	
+	private static  SchemaManager schemaManager;
 	/**
 	 * The constructor
 	 */
@@ -57,5 +83,75 @@ public class Activator extends AbstractUIPlugin {
 	 */
 	public static ImageDescriptor getImageDescriptor(String path) {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+	public static void openFileInEditor(IFile file, String id){
+		IWorkbenchWindow iwb = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		if(iwb == null) return;
+		IWorkbenchPage wp = iwb.getActivePage();
+		if(wp == null) return;
+		try {
+			wp.openEditor(new FileEditorInput(file),id);
+		} catch (PartInitException e) {
+			e.printStackTrace();
+		}
+	}
+	public static IWorkspace getWorkspace() {
+		return ResourcesPlugin.getWorkspace();
+	}
+	public static SchemaManager getSchemaManager(){
+		if(schemaManager != null)
+			return schemaManager;
+//		schemaManager = new SchemaManager();
+		schemaManager = SchemaManager.getDefaultInstance();
+		return schemaManager;
+	}
+	public static void main(String[] args){
+//		SchemaManager sm = Activator.getSchemaManager();
+//		Path path = new Path("xml/components.sxsd");
+//		String filePath;
+//		try {
+//			filePath = FileLocator.resolve(
+//					FileLocator.find(Activator.getDefault().getBundle(), path,
+//							null)).getPath();
+//
+//		sm.loadSchemaByFile(filePath);
+//		String prefix = "sc";
+//		String namespace ="http://www.uncertain-framework.org/schema/simple-schema";
+//		String local_name="grid";
+//		QualifiedName qf = new QualifiedName(prefix,namespace,local_name);
+//		Element el = sm.getElement(qf);
+//		System.out.println("el.isArray():"+el.isArray());
+////		System.out.println("el.isArray():"+el.());
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		Schema  schema = SchemaManager.getSchemaForSchema();
+		Element[] eles= schema.getElements();
+		for(int i=0;i<eles.length;i++){
+			Element ele= eles[i];
+			System.out.println(ele.getLocalName());
+		}
+		
+		
+		System.out.println(schema.getElements().length);
+		QualifiedName qn = new QualifiedName(SchemaConstant.SCHEMA_NAMESPACE, SchemaConstant.NAME_ELEMENT);
+		Element element = schema.getElement(qn);
+		if(element != null){
+			System.out.println("element..");
+			List arrays = element.getAllElements();
+			System.out.println("arrays.size():"+arrays.size());
+			if(arrays != null){
+				System.out.println("arrays..");
+		        Iterator ite = arrays.iterator();
+		        while(ite.hasNext()){
+		        	System.out.println("hasNext..");
+		        	final Element ele = (Element)ite.next();
+		        	System.out.println(ele.getName());
+				}
+			}
+		}
+		
 	}
 }
