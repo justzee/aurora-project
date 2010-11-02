822187c1177f0389c1bfc945bc9a597d4733821f
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-11-02 08:31:22 +0000

    添加了几个常量

--- uncertain_ide/aurora/ide/AuroraConstant.java
+++ uncertain_ide/aurora/ide/AuroraConstant.java
@@ -3,10 +3,16 @@
  */
 package aurora.ide;
 
+import uncertain.composite.QualifiedName;
+
+
+
+
 /**
  * @author linjinxiao
  *
  */
 public interface AuroraConstant {
 	public final static String ApplicationUri = "http://www.aurora-framework.org/application";
+	public final static QualifiedName screenQN = new QualifiedName(ApplicationUri, "screen");
 }
