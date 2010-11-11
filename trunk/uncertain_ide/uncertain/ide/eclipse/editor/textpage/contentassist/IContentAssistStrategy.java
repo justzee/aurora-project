dbe766768b2cdf6c86c1ee5619c0da3d24ebf716
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-11-11 09:01:56 +0000

    增强了内容辅助功能：可以提示下级子节点，并显示对数组和元素的图标进行区分。属性方面，过滤了已有的属性，并根据当前光标所获得的字符，自动筛选过滤属性。

---
+++ trunk/uncertain_ide/uncertain/ide/eclipse/editor/textpage/contentassist/IContentAssistStrategy.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package uncertain.ide.eclipse.editor.textpage.contentassist;
+
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+
+/**
+ * @author linjinxiao
+ * 
+ */
+public interface IContentAssistStrategy {
+
+	public ICompletionProposal[] computeCompletionProposals(ITextViewer viewer,
+			int offset) throws BadLocationException;
+}
