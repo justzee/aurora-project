8c894ca0b8bb30ba385f803897e10607a9f61317
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-11-11 09:05:00 +0000

    把固定的aurora bm uri挪位置

--- trunk/uncertain_ide/aurora/ide/AuroraConstant.java
+++ trunk/uncertain_ide/aurora/ide/AuroraConstant.java
@@ -15,4 +15,5 @@ import uncertain.composite.QualifiedName;
 public interface AuroraConstant {
 	public final static String ApplicationUri = "http://www.aurora-framework.org/application";
 	public final static QualifiedName screenQN = new QualifiedName(ApplicationUri, "screen");
+	public final static String BMUri = "http://www.aurora-framework.org/schema/bm";
 }
