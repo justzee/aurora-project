fcf520807d1628a8a3b814a746870d61e0239c8e
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-11-12 06:45:01 +0000

    删除main函数

--- trunk/uncertain_ide/uncertain/ide/Common.java
+++ trunk/uncertain_ide/uncertain/ide/Common.java
@@ -7,9 +7,6 @@ import org.eclipse.core.resources.IFile;
 
 public class Common {
 
-	public static void main(String[] args) {
-	}
-
 	public static String getIfileLocalPath(IFile ifile) {
 		String fileFullPath = ifile.getLocation().toOSString();
 		return fileFullPath;
