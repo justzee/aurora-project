5d0088cebaa52b0efc33adc062117cbf99670b03
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-12-02 06:19:55 +0000

    添加常量

--- uncertain_ide/aurora/ide/AuroraConstant.java
+++ uncertain_ide/aurora/ide/AuroraConstant.java
@@ -16,4 +16,7 @@ public interface AuroraConstant {
 	public final static String ApplicationUri = "http://www.aurora-framework.org/application";
 	public final static QualifiedName screenQN = new QualifiedName(ApplicationUri, "screen");
 	public final static String BMUri = "http://www.aurora-framework.org/schema/bm";
+	
+	public static String[] buildinFileExtension = new String[]{"screen","bm"};
+	public static String screenFileExtension = "screen";
 }
