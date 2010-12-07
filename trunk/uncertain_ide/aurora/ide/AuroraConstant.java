359971ccd60a2522faa497d6b4a348ac9fe0e614
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-12-07 07:54:05 +0000

    添加常量

--- trunk/uncertain_ide/aurora/ide/AuroraConstant.java
+++ trunk/uncertain_ide/aurora/ide/AuroraConstant.java
@@ -13,10 +13,14 @@ import uncertain.composite.QualifiedName;
  *
  */
 public interface AuroraConstant {
+	//uri
 	public final static String ApplicationUri = "http://www.aurora-framework.org/application";
-	public final static QualifiedName screenQN = new QualifiedName(ApplicationUri, "screen");
 	public final static String BMUri = "http://www.aurora-framework.org/schema/bm";
+	//qn
+	public final static QualifiedName screenQN = new QualifiedName(ApplicationUri, "screen");
+	public static QualifiedName modelQN = new QualifiedName(BMUri, "model");
 	
 	public static String[] buildinFileExtension = new String[]{"screen","bm"};
 	public static String screenFileExtension = "screen";
+
 }
