8099c1eb1c8815f56a1a16555b6b96d42eabd7aa
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2011-01-12 02:21:09 +0000

    如果中途取消向导，不需要更新

--- uncertain_ide/uncertain/ide/eclipse/action/AddDataSetAction.java
+++ uncertain_ide/uncertain/ide/eclipse/action/AddDataSetAction.java
@@ -1,6 +1,7 @@
 package uncertain.ide.eclipse.action;
 
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Shell;
 
@@ -13,27 +14,32 @@ import uncertain.ide.util.LocaleMessage;
 public class AddDataSetAction extends AddElementAction {
 
 	public AddDataSetAction(IViewer viewer, CompositeMap parentCM,
-			String prefix, String uri, String cmName,String text, int actionStyle) {
-		super(viewer,parentCM,prefix,uri,cmName,text,actionStyle);
+			String prefix, String uri, String cmName, String text,
+			int actionStyle) {
+		super(viewer, parentCM, prefix, uri, cmName, text, actionStyle);
 	}
 
-	public AddDataSetAction(IViewer viewer, CompositeMap parentCM,QualifiedName qName,int actionStyle) {
-		super(viewer,parentCM,qName,actionStyle);
+	public AddDataSetAction(IViewer viewer, CompositeMap parentCM,
+			QualifiedName qName, int actionStyle) {
+		super(viewer, parentCM, qName, actionStyle);
 	}
 
 	public void run() {
-		
-		callWizard();
+		if (callWizard() == Window.CANCEL)
+			return;
 		if (viewer != null) {
 			viewer.refresh(true);
 		}
 	}
-	private void callWizard() {
-		DataSetWizard  wizard = new DataSetWizard(parent,prefix,uri,localName);
-		WizardDialog dialog = new WizardDialog(new Shell(),wizard);
-		dialog.open();
+
+	private int callWizard() {
+		DataSetWizard wizard = new DataSetWizard(parent, prefix, uri, localName);
+		WizardDialog dialog = new WizardDialog(new Shell(), wizard);
+		return dialog.open();
 	}
+
 	public ImageDescriptor getDefaultImageDescriptor() {
-		return Activator.getImageDescriptor(LocaleMessage.getString("wizard.icon"));
+		return Activator.getImageDescriptor(LocaleMessage
+				.getString("wizard.icon"));
 	}
 }
