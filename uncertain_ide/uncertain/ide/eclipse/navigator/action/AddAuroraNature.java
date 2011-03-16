0b270a6da6dbe726eddf6ce3c9399586e001af04
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2011-03-16 13:34:15 +0000

    1 完美注释 2 完美内容提示 3 双击高亮导航 4 实时错误提示 5 创建Lov和Combo向导 6 自动注册字段描述 7 完善dataset创建form和gird

---
+++ uncertain_ide/uncertain/ide/eclipse/navigator/action/AddAuroraNature.java
@@ -0,0 +1,48 @@
+package uncertain.ide.eclipse.navigator.action;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+import uncertain.ide.AuroraProjectNature;
+import uncertain.ide.help.CustomDialog;
+
+public class AddAuroraNature implements IObjectActionDelegate {
+
+	ISelection selection;
+
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+	}
+	public void run(IAction action) {
+		if (!(selection instanceof IStructuredSelection)){
+			CustomDialog.showErrorMessageBox(selection+"is not a IStructuredSelection!");
+			return;
+		}
+		IStructuredSelection structured = (IStructuredSelection) selection;
+		Object firstElment = structured.getFirstElement();
+		if (!(firstElment instanceof IProject)){
+			CustomDialog.showErrorMessageBox(firstElment+"is not a IProject!");
+			return;
+		}
+		IProject project = (IProject) firstElment;
+		if(!project.isOpen()){
+			return;
+		}
+		try {
+			if(!AuroraProjectNature.hasAuroraNature(project)){
+				AuroraProjectNature.addAuroraNature(project);
+			}
+		} catch (CoreException e) {
+			CustomDialog.showErrorMessageBox(e);
+		}
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		this.selection = selection;
+
+	}
+}
