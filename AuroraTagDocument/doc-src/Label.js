575a7775a0e910e6de5cd1abbbc4a7b340959fa1
Hugh.HZ.Wu <Hugh.HZ.Wu@c3805726-bc34-11dd-8164-2ff6f70dce53>
2011-04-13 02:29:35 +0000

    增加Label至AuroraTagDocument

---
+++ AuroraTagDocument/doc-src/Label.js
@@ -0,0 +1,15 @@
+/**
+ * @class Label
+ * @namespace Abstract
+ * @extends Component
+ * @author 牛佳庆
+ */
+ 
+ 
+/**
+ * <p>渲染</p> 
+ * <p>回调函数function(value,record,name){return value}</p>
+ * <p>返回值value是html字符串</p>
+ * @property renderer
+ * @type Function
+ */
