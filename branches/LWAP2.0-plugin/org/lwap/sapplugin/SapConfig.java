200b51d777169a0a7075e8a942aa5c24aaa019e4
bobbie.zou@gmail.com <bobbie.zou@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2011-04-22 08:16:43 +0000

    添加default sap instance

--- branches/LWAP2.0-plugin/org/lwap/sapplugin/SapConfig.java
+++ branches/LWAP2.0-plugin/org/lwap/sapplugin/SapConfig.java
@@ -7,15 +7,21 @@ import uncertain.core.IGlobalInstance;
 
 public class SapConfig implements IGlobalInstance{
 	Map sapInstanceMap=new HashMap();
-	
+	String defaultKey=null;
 	public SapInstance getSapInstance(String sid){
 		return (SapInstance)sapInstanceMap.get(sid);
+	}
+	
+	public SapInstance getDefaultSapInstance(){
+		return (SapInstance)sapInstanceMap.get(defaultKey);
 	}
 
 	public void addInstances(SapInstance[] instances) {
 		SapInstance instance;
 		for(int i=0,l=instances.length;i<l;i++){
 			instance=instances[i];
+			if(defaultKey==null)
+				defaultKey=instance.SID;
 			sapInstanceMap.put(instance.SID, instance);
 		}
 	}
