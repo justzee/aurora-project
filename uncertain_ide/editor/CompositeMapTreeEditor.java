65dfbd72b7b61c8664853702f32bf6cf9f01a2f2
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2011-06-07 06:49:06 +0000

    1 支持根据数据库package创建BM 2 添加新建文件头署名 3 新增js代码格式化功能 4 支持属性内容的换行 5 支持在线安装，并分离IDE和依赖包 6 创建文件时，添加是否重名的判断 7 其他各种改进

---
+++ uncertain_ide/editor/CompositeMapTreeEditor.java
@@ -0,0 +1,76 @@
+package editor;
+
+import helpers.CompositeMapLocatorParser;
+import helpers.DialogUtil;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.eclipse.jface.viewers.StructuredSelection;
+
+import uncertain.composite.CompositeMap;
+
+
+public abstract class CompositeMapTreeEditor extends BaseCompositeMapEditor {
+
+	
+	protected CompositeMapTreePage treePage;
+	
+	public CompositeMapTreeEditor() {
+		super();
+	}
+	
+	public CompositeMapPage initMainViewerPage(){
+		this.treePage = initTreePage();
+		return treePage;
+	}
+	public abstract CompositeMapTreePage initTreePage();
+	
+	protected void pageChange(int newPageIndex){
+		int currentPage = getCurrentPage();
+		super.pageChange(newPageIndex);
+		if(currentPage==mainViewerIndex&&newPageIndex ==textPageIndex){
+			locateTextPage();
+		}else if(currentPage==textPageIndex&&newPageIndex ==mainViewerIndex){
+			locateTreePage();
+		}
+	}
+	private void locateTreePage(){
+		CompositeMapLocatorParser parser = new CompositeMapLocatorParser();
+
+		try {
+			InputStream content = new ByteArrayInputStream(textPage.getContent()
+					.getBytes("UTF-8"));
+			CompositeMap  cm = parser.getCompositeMapFromLine(content, textPage.getCursorLine());
+			if(cm != null){
+//				treePage.getTreeViewer().expandToLevel(
+//						cm, 0);
+				treePage.getTreeViewer().setSelection(
+						new StructuredSelection(cm), true);
+			}
+		} catch (Exception e){
+			DialogUtil.showExceptionMessageBox(e);
+		}
+
+	}
+	private void locateTextPage(){
+		CompositeMap selection = treePage.getSelection();
+		if(selection == null)
+			return;
+		CompositeMapLocatorParser parser = new CompositeMapLocatorParser();
+
+		int line = 0;
+		try {
+			InputStream content = new ByteArrayInputStream(textPage.getContent().getBytes("UTF-8"));
+			line = parser.LocateCompositeMapLine(content, selection);
+		} catch (Exception e) {
+			DialogUtil.showExceptionMessageBox(e);
+		}
+		int offset = textPage.getOffsetFromLine(line);
+		int length = textPage.getLengthOfLine(line);
+		if(offset==0||length==0)
+			return;
+		textPage.resetHighlightRange();
+		textPage.setHighlightRange(offset, length, true);
+	}
+}
