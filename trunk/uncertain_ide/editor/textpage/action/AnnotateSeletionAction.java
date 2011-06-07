1ece21f82f4a746643e4331d8d46ff833f66e364
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2011-06-07 07:46:45 +0000

    1 支持根据数据库package创建BM 2 添加新建文件头署名 3 新增js代码格式化功能 4 支持属性内容的换行 5 支持在线安装，并分离IDE和依赖包 6 创建文件时，添加是否重名的判断 7 其他各种改进

--- trunk/uncertain_ide/editor/textpage/action/AnnotateSeletionAction.java
+++ trunk/uncertain_ide/editor/textpage/action/AnnotateSeletionAction.java
@@ -1,4 +1,6 @@
-package uncertain.ide.eclipse.editor.textpage.action;
+package editor.textpage.action;
+
+import helpers.DialogUtil;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.text.BadLocationException;
@@ -9,9 +11,9 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorPart;
 
-import uncertain.ide.eclipse.editor.textpage.TextPage;
-import uncertain.ide.eclipse.editor.textpage.scanners.XMLPartitionScanner;
-import uncertain.ide.help.CustomDialog;
+import editor.textpage.TextPage;
+import editor.textpage.scanners.XMLPartitionScanner;
+
 
 public class AnnotateSeletionAction implements IEditorActionDelegate {
 
@@ -26,7 +28,7 @@ public class AnnotateSeletionAction implements IEditorActionDelegate {
 
 	public void run(IAction action) {
 		if (activeEditor == null || !(activeEditor instanceof TextPage)) {
-			CustomDialog.showErrorMessageBox("这个类不是" + TextPage.class.getName());
+			DialogUtil.showErrorMessageBox("这个类不是" + TextPage.class.getName());
 			return;
 		}
 		TextPage tp = (TextPage) activeEditor;
@@ -40,14 +42,14 @@ public class AnnotateSeletionAction implements IEditorActionDelegate {
 			return ;
 		try {
 			ITypedRegion  partitionRegion = document.getPartition(ts.getOffset());
-			if(XMLPartitionScanner.XML_TEXT.equals(partitionRegion.getType())){
+			if(XMLPartitionScanner.XML_CDATA.equals(partitionRegion.getType())){
 				content ="/*"+content+"*/";
 			}else if( XMLPartitionScanner.XML_START_TAG.equals(partitionRegion.getType())|| IDocument.DEFAULT_CONTENT_TYPE.equals(partitionRegion.getType())){
 				content ="<!--"+content+"-->";
 			}
 			document.replace(ts.getOffset(), ts.getLength(), content);
 		} catch (BadLocationException e) {
-			CustomDialog.showErrorMessageBox(e);
+			DialogUtil.showExceptionMessageBox(e);
 			return ;
 		}
 
