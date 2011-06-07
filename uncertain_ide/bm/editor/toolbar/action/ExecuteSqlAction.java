2d9c9ce6597a877190e38c561f0b5b24c4cc4c44
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2011-06-07 06:38:46 +0000

    1 支持根据数据库package创建BM 2 添加新建文件头署名 3 新增js代码格式化功能 4 支持属性内容的换行 5 支持在线安装，并分离IDE和依赖包 6 创建文件时，添加是否重名的判断 7 其他各种改进

---
+++ uncertain_ide/bm/editor/toolbar/action/ExecuteSqlAction.java
@@ -0,0 +1,63 @@
+package bm.editor.toolbar.action;
+
+import helpers.DialogUtil;
+import helpers.LocaleMessage;
+import ide.AuroraPlugin;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.resource.ImageDescriptor;
+
+import editor.core.ISqlViewer;
+
+
+public class ExecuteSqlAction extends Action {
+	ISqlViewer viewer;
+
+	public ExecuteSqlAction(ISqlViewer viewer) {
+		this.viewer = viewer;
+	}
+
+	public ExecuteSqlAction(ISqlViewer viewer,ImageDescriptor imageDescriptor, String text) {
+		if (imageDescriptor != null)
+			setHoverImageDescriptor(imageDescriptor);
+		if (text != null)
+			setText(text);
+		this.viewer = viewer;
+	}
+
+	public void run() {
+		Connection conn = viewer.getConnection();
+		String sql = viewer.getSql();
+
+//		String action = viewer.getAction();
+		String action = sql.split(" ")[0];
+		ResultSet resultSet = null;
+		Statement stmt;
+		int resultCount = 0;
+		try {
+			stmt = conn.createStatement();
+			if ("select".equalsIgnoreCase(action)) {
+				resultSet = stmt.executeQuery(sql);
+			}
+			else if(action != null){
+				resultCount = stmt.executeUpdate(sql);
+			}
+			if(resultSet != null){
+				resultCount = resultSet.getFetchSize();
+			}
+		} catch (SQLException e) {
+			DialogUtil.showExceptionMessageBox(e);
+			return;
+		}
+		viewer.refresh(resultSet, resultCount);
+	}
+
+	public static ImageDescriptor getDefaultImageDescriptor() {
+		return AuroraPlugin.getImageDescriptor(LocaleMessage.getString("run.icon"));
+	}
+}
