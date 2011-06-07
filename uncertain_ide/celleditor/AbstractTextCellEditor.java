94334037c095233def3863307e440039b94788c9
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2011-06-07 06:46:03 +0000

    1 支持根据数据库package创建BM 2 添加新建文件头署名 3 新增js代码格式化功能 4 支持属性内容的换行 5 支持在线安装，并分离IDE和依赖包 6 创建文件时，添加是否重名的判断 7 其他各种改进

---
+++ uncertain_ide/celleditor/AbstractTextCellEditor.java
@@ -0,0 +1,132 @@
+package celleditor;
+
+import helpers.DialogUtil;
+import helpers.LocaleMessage;
+
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.ICellEditorListener;
+import org.eclipse.jface.viewers.TextCellEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.TableEditor;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.FocusListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
+
+
+public abstract class AbstractTextCellEditor extends TextCellEditor implements
+		ICellEditor {
+
+	protected CellInfo cellProperties;
+	protected String oldInput;
+	public AbstractTextCellEditor(CellInfo cellProperties) {
+		this.cellProperties = cellProperties;
+	}
+
+	public boolean validValue(String value) {
+		if(cellProperties.isRequired() && (value == null || value.equals(""))){
+			String message = "<"+cellProperties.getColumnName()+"> "+LocaleMessage.getString("field")+LocaleMessage.getString("is.required");
+			setErrorMessage(message);
+			getCellControl().setFocus();
+			return false;
+		}
+		return true;
+	}
+
+	public Control getCellControl() {
+		return super.getControl();
+	}
+
+	public String getSelection() {
+		Object value = getValue();
+		if(value == null)
+			return null;
+		return value.toString();
+	}
+	public Object valueToShow(String value){
+		return value;
+	}
+	public void SetSelection(String value) {
+		if(value != null)
+			super.setValue(value);
+	}
+
+	public void dispose() {
+		super.dispose();
+
+	}
+
+	public void init() {
+		if(getCellControl()!=null){
+			return;
+		}
+		Color bg = Display.getDefault().getSystemColor(SWT.COLOR_YELLOW);
+		Table parent = cellProperties.getTable();
+		createCellEditor(parent);
+		if (cellProperties.isRequired()) {
+			getCellControl().setBackground(bg);
+		}
+		if(isTableItemEditor()){
+			SetSelection(cellProperties.getRecord().getString(cellProperties.getColumnName()));
+		}
+		addCellListener();
+		oldInput = getSelection();
+	}
+	
+	protected void addCellListener() {
+		if(!isTableItemEditor())
+			return ;
+		this.addListener(new ICellEditorListener() {
+
+			public void editorValueChanged(boolean oldValidState,
+					boolean newValidState) {
+				cellProperties.getTableViewer().refresh(true);
+				String input = getSelection();
+				 if(validValue(input)){
+					 oldInput = input;
+				 }else{
+					 DialogUtil.showErrorMessageBox(getErrorMessage());
+					 SetSelection(oldInput);
+				 }
+			}
+
+			public void cancelEditor() {
+			}
+
+			public void applyEditorValue() {
+				String dataValue = getSelection();
+				cellProperties.getRecord().put(cellProperties.getColumnName(), dataValue);
+			}
+		});
+		getCellControl().addFocusListener(new FocusListener() {
+			
+			public void focusLost(FocusEvent e) {
+				if(isTableItemEditor()){
+					fillTableCellEditor(cellProperties.getTableItem());
+				}
+			}
+			
+			public void focusGained(FocusEvent e) {
+			}
+		});
+	}
+	protected void fillTableCellEditor(TableItem item) {
+		TableEditor editor = new TableEditor(item.getParent());
+		editor.horizontalAlignment = SWT.LEFT;
+		editor.grabHorizontal = true;
+		editor.setEditor(getCellControl(), item, 1);
+	}
+	protected boolean dependsOnExternalFocusListener() {
+		return false;
+	}
+	public CellEditor getCellEditor(){
+		return this;
+	}
+	private boolean isTableItemEditor(){
+		return cellProperties.getTableItem() != null;
+	}
+	
+}
