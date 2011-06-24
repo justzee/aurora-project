4dcdb35735b29ccbecbefe05805abb6b6d9860be
linjinxiao@gmail.com <linjinxiao@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2011-06-24 02:02:39 +0000

    丰富了测试例子

--- trunk/uncertain_test/uncertain/testcase/exception/MessageFactoryTest.java
+++ trunk/uncertain_test/uncertain/testcase/exception/MessageFactoryTest.java
@@ -1,8 +1,9 @@
 package uncertain.testcase.exception;
 
-
+import java.io.IOException;
 import java.util.Locale;
 
+import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
 import junit.framework.TestCase;
@@ -10,37 +11,58 @@ import uncertain.composite.CompositeLoader;
 import uncertain.exception.GeneralException;
 import uncertain.exception.MessageFactory;
 
-public class MessageFactoryTest  extends TestCase {
+public class MessageFactoryTest extends TestCase {
 
-	public void testLoadResource(){
-		String resourceFileName="exceptionMessage";
-		String path = MessageFactoryTest.class.getPackage().getName()+"."+resourceFileName;
+	public void testLoadResource() {
+		String resourceFileName = "exceptionMessage";
+		String path = MessageFactoryTest.class.getPackage().getName() + "." + resourceFileName;
 		MessageFactory.loadResource(path);
-		String messageCode="aurora00001";
+		String messageCode = "aurora00001";
 		assertEquals("这是一个测试案例!", MessageFactory.getMessage(messageCode, null));
-		MessageFactory.loadResource(path,Locale.US);
-		assertEquals("this is test case.", MessageFactory.getMessage(messageCode, Locale.US,null));
+		MessageFactory.loadResource(path, Locale.US);
+		assertEquals("this is test case.", MessageFactory.getMessage(messageCode, Locale.US, null));
 		assertEquals("这是一个测试案例!", MessageFactory.getMessage(messageCode, null));
 	}
-	public void testCreateException(){
+
+	public void testCreateException() {
 		CompositeLoader cl = CompositeLoader.createInstanceForOCM();
-		String bmFile = "error_format";
-		try{
-			try {
-				cl.loadFromClassPath(MessageFactoryTest.class.getPackage().getName()+"."+bmFile, "bm");
-			} catch (Throwable e) {
-				GeneralException ge= MessageFactory.createException("aurora00002", e, new String[]{"格式错误","格式正确"});
-				throw ge;
-			} 
+		String notExistFile = "not_Exist";
+		String errorFormatFile = "error_format";
+		try {
+			LoadCompositeMap(notExistFile);
 		} catch (Throwable e) {
-			assertEquals("aurora00002:不能是格式错误，必须是格式正确.",e.getMessage());
-			SAXParseException parseEx = (SAXParseException) e.getCause();
-			assertEquals(11,parseEx.getLineNumber());
+			assertEquals("aurora00002:not_Exist.bm文件不存在!", e.getMessage());
 		}
-			
-	}
-	public static void main(String[] args){
-		(new MessageFactoryTest()).testLoadResource();
+		try {
+			LoadCompositeMap(errorFormatFile);
+		} catch (Throwable e) {
+			assertEquals("aurora00004:error_format.bm文件格式不正确，请检查第11行第69列!", e.getMessage());
+		}
 		
+	}
+
+	private void LoadCompositeMap(String fileName) {
+		CompositeLoader cl = CompositeLoader.createInstanceForOCM();
+		try {
+			cl.loadFromClassPath(MessageFactoryTest.class.getPackage().getName() + "." + fileName, "bm");
+		} catch (IOException e) {
+			GeneralException ge = MessageFactory.createException("aurora00002", e, new String[] { fileName + ".bm" });
+			throw ge;
+		} catch (SAXException e) {
+			GeneralException ge = null;
+			if (e instanceof SAXParseException) {
+				SAXParseException parseEx = (SAXParseException) e;
+				ge = MessageFactory.createException("aurora00004", e, new String[] { fileName + ".bm",
+						String.valueOf(parseEx.getLineNumber()), String.valueOf(parseEx.getColumnNumber()) });
+			} else {
+				ge = MessageFactory.createException("aurora00003", e, new String[] { fileName + ".bm" });
+			}
+			throw ge;
+		}
+	}
+
+	public static void main(String[] args) {
+		(new MessageFactoryTest()).testLoadResource();
+
 	}
 }
