09bde2d2e9bb1852b88949578dcaf2d45fbcc1c7
linjinxiao@gmail.com <linjinxiao@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2011-07-04 05:44:14 +0000

    1 在此类中如果再发生异常，就用system.err抛出，避免死循环。 2 添加配置文件样例 3 用DBUtil来关闭statement，connection

--- branches/uncertain-exceptionRecord/aurora/aurora/service/exception/ExceptionDatabaseLog.java
+++ branches/uncertain-exceptionRecord/aurora/aurora/service/exception/ExceptionDatabaseLog.java
@@ -4,7 +4,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
+
+import javax.sql.DataSource;
 
 import uncertain.composite.CompositeMap;
 import uncertain.core.ConfigurationError;
@@ -12,23 +13,44 @@ import uncertain.core.IGlobalInstance;
 import uncertain.core.UncertainEngine;
 import uncertain.exception.IExceptionListener;
 import uncertain.exception.IExceptionWithContext;
-import aurora.database.service.DatabaseServiceFactory;
-import aurora.database.service.SqlServiceContext;
+import aurora.database.DBUtil;
 import aurora.service.ServiceThreadLocal;
 
-public class ExceptionDatabaseLog implements IExceptionListener,IGlobalInstance {
+/*
+ *         配置文件样例
+ <exception-database-log xmlns="aurora.service.exception">
+	 <!-- 定义哪些异常是可以忽略的 -->
+	 <ignored-types>
+		 <ignored-type name="uncertain.core.ConfigurationError" />
+		 <ignored-type name="uncertain.exception.GeneralException" />
+	 </ignored-types>
+	
+	 <!-- 定义创建异常记录的SQL语句 -->
+	 <insert-sql>
+		 begin
+		 insert into fnd_exception_logs ( id,exception_type, exception_message,source ,context ,root_stack_trace,full_stack_trace)
+		 values (fnd_exception_logs_s.nextval, ?, ?, ?, ?,?,? );
+		 end;
+	 </insert-sql>
+ </exception-database-log>	
+ * 
+ */
+public class ExceptionDatabaseLog implements IExceptionListener, IGlobalInstance {
 
 	private IgnoredType[] ignoredTypes;
 	private UncertainEngine mUncertainEngine;
 	private String sql;
 	private CompositeMap sqlNode;
+
 	public ExceptionDatabaseLog(UncertainEngine engine) {
 		mUncertainEngine = engine;
 	}
-	public void addIgnoredTypes(IgnoredType[] ignoredTypes){
+
+	public void addIgnoredTypes(IgnoredType[] ignoredTypes) {
 		this.ignoredTypes = ignoredTypes;
 	}
-	public void setInsertSql(CompositeMap sqlNode){
+
+	public void setInsertSql(CompositeMap sqlNode) {
 		this.sqlNode = sqlNode;
 		if (sqlNode == null) {
 			throw new ConfigurationError("Must has insert-sql node!");
@@ -38,30 +60,30 @@ public class ExceptionDatabaseLog implements IExceptionListener,IGlobalInstance 
 			throw new ConfigurationError("sql can not be empty!");
 		}
 	}
-	public CompositeMap getInsertSql(){
+
+	public CompositeMap getInsertSql() {
 		return sqlNode;
 	}
 
 	public void onException(Throwable exception) {
 		if (exception == null)
 			return;
-		if(ignoredTypes != null){
-			for(int i=0;i<ignoredTypes.length;i++){
+		if (ignoredTypes != null) {
+			for (int i = 0; i < ignoredTypes.length; i++) {
 				if (exception.getClass().getCanonicalName().equals(ignoredTypes[i].getName())) {
 					return;
 				}
 			}
 		}
-		DatabaseServiceFactory databasefactory = (DatabaseServiceFactory) mUncertainEngine.getObjectRegistry().getInstanceOfType(DatabaseServiceFactory.class);
-		SqlServiceContext ssc = null;
+		DataSource ds = (DataSource) mUncertainEngine.getObjectRegistry().getInstanceOfType(DataSource.class);
+		Connection conn = null;
 		PreparedStatement ps = null;
 		try {
-			ssc = databasefactory.createContextWithConnection();
-			Connection conn = ssc.getConnection();
+			conn = ds.getConnection();
 			ps = conn.prepareStatement(sql);
 			ps.setString(1, exception.getClass().getCanonicalName());
 			ps.setString(2, exception.getMessage());
-			ps.setString(3, getSource()); 
+			ps.setString(3, getSource());
 			String context = getContext(exception);
 			ps.setString(4, context);
 			String rootStackTrace = getRootStackTrace(exception);
@@ -71,63 +93,61 @@ public class ExceptionDatabaseLog implements IExceptionListener,IGlobalInstance 
 			ps.executeUpdate();
 			ps.close();
 		} catch (Throwable e) {
-			throw new ConfigurationError(e);
+			e.printStackTrace();
 		} finally {
-			try {
-				if (ps != null) {
-					ps.close();
-				}
-				if(ssc != null){
-					ssc.freeConnection();
-				}
-			} catch (SQLException e) {
-				mUncertainEngine.logException("close DataBase resource failed.", e);
-			}
+			DBUtil.closeStatement(ps);
+			DBUtil.closeConnection(conn);
 		}
 	}
-	private String getSource(){
-		if(ServiceThreadLocal.getSource()!= null)
+
+	private String getSource() {
+		if (ServiceThreadLocal.getSource() != null)
 			return ServiceThreadLocal.getSource();
 		return "";
 	}
-	private String getContext(Throwable exception){
+
+	private String getContext(Throwable exception) {
 		CompositeMap context;
-		if(exception instanceof IExceptionWithContext ){
-			IExceptionWithContext e = (IExceptionWithContext)exception;
+		if (exception instanceof IExceptionWithContext) {
+			IExceptionWithContext e = (IExceptionWithContext) exception;
 			context = e.getExceptionContext();
-			if(context!= null){
+			if (context != null) {
 				return context.toXML();
 			}
 		}
 		context = ServiceThreadLocal.getCurrentThreadContext();
-		if(context!= null)
+		if (context != null)
 			return context.toXML();
 		return null;
 	}
-	private String getRootStackTrace(Throwable exception){
+
+	private String getRootStackTrace(Throwable exception) {
 		Throwable rootCause = getRootCause(exception);
 		return getExceptionStackTrace(rootCause);
-		
+
 	}
-	private String getFullStackTrace(Throwable exception){
+
+	private String getFullStackTrace(Throwable exception) {
 		return getExceptionStackTrace(exception);
-		
+
 	}
-	private String getExceptionStackTrace(Throwable exception){
-		if(exception == null)
+
+	private String getExceptionStackTrace(Throwable exception) {
+		if (exception == null)
 			return null;
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		PrintStream pw = new PrintStream(baos);
 		exception.printStackTrace(pw);
 		pw.close();
 		return baos.toString();
-		
+
 	}
-	private Throwable getRootCause(Throwable exception){
-		if(exception == null)
+
+	private Throwable getRootCause(Throwable exception) {
+		if (exception == null)
 			return exception;
 		Throwable cause = exception.getCause();
-		if(cause == null)
+		if (cause == null)
 			return exception;
 		return getRootCause(cause);
 	}
