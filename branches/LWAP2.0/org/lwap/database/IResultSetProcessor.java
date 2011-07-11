bdb9fbe20d746dd282a6d141b1a63a0fb8ed9a58
seacat.zhou@gmail.com <seacat.zhou@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2011-07-11 04:44:23 +0000

    Add IResultSetProcessor feature

---
+++ branches/LWAP2.0/org/lwap/database/IResultSetProcessor.java
@@ -0,0 +1,13 @@
+/*
+ * Created on 2011-7-11 上午11:55:06
+ * $Id$
+ */
+package org.lwap.database;
+
+import java.sql.ResultSet;
+
+public interface IResultSetProcessor {
+    
+    public void processResultSet( ResultSet rs );
+
+}
