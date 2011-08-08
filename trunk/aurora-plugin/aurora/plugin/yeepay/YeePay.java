ebdeed2303c5a98e63a2ea4037b4a851d82e57b9
rufus.sly@gmail.com <rufus.sly@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2011-08-08 08:41:02 +0000

    yeepay退款

--- trunk/aurora-plugin/aurora/plugin/yeepay/YeePay.java
+++ trunk/aurora-plugin/aurora/plugin/yeepay/YeePay.java
@@ -9,8 +9,8 @@ public class YeePay {
 	public static final String KEY_P0_CMD = "p0_Cmd";
 	public static final String KEY_P1_MER_ID = "p1_MerId";
 	public static final String KEY_P2_ORDER = "p2_Order";
-	public static final String KEY_P3_ATM = "p3_Amt";
-	public static final String KEY_P4_CUR = "p4_Cur";
+	public static final String KEY_P3_ATM = "p3_Amt";//退款,付款金额
+	public static final String KEY_P4_CUR = "p4_Cur";//交易币种
 	public static final String KEY_P5_PID = "p5_Pid";
 	public static final String KEY_P6_PCAT = "p6_Pcat";
 	public static final String KEY_P7_PDESC = "p7_Pdesc";
@@ -21,6 +21,13 @@ public class YeePay {
 	public static final String KEY_NEED_RESPONSE = "pr_NeedResponse";
 	public static final String KEY_HMAC = "hmac";
 	public static final String KEY_URL = "nodeAuthorizationURL";
+	public static final String KEY_P5_DESC ="p5_Desc";//退款说明
+	public static final String KEY_PB_TRXID ="pb_TrxId";//易宝交易流水号
+	public static final String KEY_R0_CMD ="r0_Cmd";//业务类型 
+	public static final String KEY_R1_CODE ="r1_Code";//查询结果
+	public static final String KEY_R2_TRXID ="r2_TrxId";//易宝支付交易流水号
+	public static final String KEY_R3_AMT ="r3_Amt";//支付金额
+	public static final String KEY_R4_CUR ="r4_Cur";//交易币种
 
 	public static String formatString(String text) {
 		if (text == null) {
