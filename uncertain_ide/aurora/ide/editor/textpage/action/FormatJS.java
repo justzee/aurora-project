69d0992ac91af6af290a8b4f1d23b2646bfe6c06
jeschou.zsy <jeschou.zsy@gmail.com>
2011-10-14 10:50:56 +0000

    1.修正script标签判断方式,改用正则表达式,可以识别各种script标签 2.修正缩进 	由固定的12空格改为动态的,缩进大小由script标签所在行的缩进加上4个空格得出 	修正script结束标签的缩进,改为和起始标签对齐

--- uncertain_ide/aurora/ide/editor/textpage/action/FormatJS.java
+++ uncertain_ide/aurora/ide/editor/textpage/action/FormatJS.java
@@ -1,6 +1,5 @@
 package aurora.ide.editor.textpage.action;
 
-
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
@@ -9,64 +8,68 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorPart;
 
+import uncertain.composite.XMLOutputter;
 import aurora.ide.editor.textpage.TextPage;
 import aurora.ide.editor.textpage.format.JSBeautifier;
 import aurora.ide.editor.textpage.scanners.XMLPartitionScanner;
 import aurora.ide.helpers.DialogUtil;
 
-import uncertain.composite.XMLOutputter;
-
 public class FormatJS implements IEditorActionDelegate {
 
-	IEditorPart activeEditor ;
-	public FormatJS() {
-	}
+    IEditorPart activeEditor;
 
-	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
-		activeEditor = targetEditor;
-	}
+    public FormatJS() {
+    }
 
-	public void run(IAction action) {
-		if(activeEditor == null||!(activeEditor instanceof TextPage)){
-			DialogUtil.showErrorMessageBox("这个类不是"+TextPage.class.getName());
-			return;
-		}
-		TextPage tp = (TextPage)activeEditor;
-		IDocument document = tp.getInputDocument();
-		int cursorLine = tp.getCursorLine();
-		try {
-			ITypedRegion region = document.getPartition(tp.getSelectedRange().x);
-			ITypedRegion parentRegion = document.getPartition(region.getOffset()-1);
-			String parentNode = document.get(parentRegion.getOffset(), parentRegion.getLength());
-			if(!XMLPartitionScanner.XML_CDATA.equals(region.getType())||!"<script>".equalsIgnoreCase(parentNode)){
-				DialogUtil.showErrorMessageBox("此区域非javascript代码");
-				return;
-			}
-			int begin = region.getOffset()+"<![CDATA[".length();
-			int length = region.getLength()-"<![CDATA[".length()-"]]>".length();
-			String jsCode = document.get(begin,length);
-			if(jsCode == null || "".equals(jsCode))
-				return;
-			JSBeautifier bf = new JSBeautifier();
-			String indent = XMLOutputter.DEFAULT_INDENT+XMLOutputter.DEFAULT_INDENT+XMLOutputter.DEFAULT_INDENT;
-			jsCode = ("\n"+bf.beautify(jsCode, bf.opts)).replaceAll("\n", "\n"+indent);
-			document.replace(begin, length, jsCode);
-		} catch (Throwable e) {
-			DialogUtil.showExceptionMessageBox(e);
-			return ;
-		}
-		try {
-			int offset = document.getLineOffset(cursorLine);
-			int length = document.getLineLength(cursorLine);
-			if(offset==0||length==0)
-				return;
-			tp.setHighlightRange(offset, length, true);
-		} catch (BadLocationException e) {
-			DialogUtil.showExceptionMessageBox(e);
-		}
-	}
+    public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+        activeEditor = targetEditor;
+    }
 
-	public void selectionChanged(IAction action, ISelection selection) {
-	}
+    public void run(IAction action) {
+        if (activeEditor == null || !(activeEditor instanceof TextPage)) {
+            DialogUtil.showErrorMessageBox("这个类不是" + TextPage.class.getName());
+            return;
+        }
+        TextPage tp = (TextPage) activeEditor;
+        IDocument document = tp.getInputDocument();
+        int cursorLine = tp.getCursorLine();
+        try {
+            ITypedRegion region = document.getPartition(tp.getSelectedRange().x);
+            ITypedRegion parentRegion = document.getPartition(region.getOffset() - 1);
+            String parentNode = document.get(parentRegion.getOffset(), parentRegion.getLength());
+            int startLine = document.getLineOfOffset(parentRegion.getOffset());
+            String txt = document.get(document.getLineOffset(startLine), document.getLineLength(startLine));
+            String prefix = txt.substring(0, txt.toLowerCase().indexOf("<script"));
+            if (!XMLPartitionScanner.XML_CDATA.equals(region.getType())
+                    || !parentNode.toLowerCase().matches("<script( .*){0,1}>")) {
+                DialogUtil.showErrorMessageBox("此区域非javascript代码");
+                return;
+            }
+            int begin = region.getOffset() + "<![CDATA[".length();
+            int length = region.getLength() - "<![CDATA[".length() - "]]>".length();
+            String jsCode = document.get(begin, length);
+            if (jsCode == null || jsCode.trim().length() == 0)
+                return;
+            JSBeautifier bf = new JSBeautifier();
+            String indent = XMLOutputter.DEFAULT_INDENT + prefix;
+            jsCode = ("\n" + bf.beautify(jsCode, bf.opts)).replaceAll("\n", "\n" + indent) + "\n" + prefix;
+            document.replace(begin, length, jsCode);
+        } catch (Throwable e) {
+            DialogUtil.showExceptionMessageBox(e);
+            return;
+        }
+        try {
+            int offset = document.getLineOffset(cursorLine);
+            int length = document.getLineLength(cursorLine);
+            if (offset == 0 || length == 0)
+                return;
+            tp.setHighlightRange(offset, length, true);
+        } catch (BadLocationException e) {
+            DialogUtil.showExceptionMessageBox(e);
+        }
+    }
+
+    public void selectionChanged(IAction action, ISelection selection) {
+    }
 
 }
