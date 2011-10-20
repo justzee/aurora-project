842a10c2433d1d467cc7c143d1d3e95f1033ebfe
jeschou.zsy <jeschou.zsy@gmail.com>
2011-10-20 08:56:31 +0000

    实现方法getAutoEditStrategies 使用新的自动编辑策略

--- uncertain_ide/aurora/ide/editor/textpage/JavaScriptConfiguration.java
+++ uncertain_ide/aurora/ide/editor/textpage/JavaScriptConfiguration.java
@@ -1,5 +1,6 @@
 package aurora.ide.editor.textpage;
 
+import org.eclipse.jface.text.IAutoEditStrategy;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextDoubleClickStrategy;
 import org.eclipse.jface.text.TextAttribute;
@@ -12,46 +13,46 @@ import org.eclipse.jface.text.source.SourceViewerConfiguration;
 
 import aurora.ide.editor.textpage.scanners.JSEditorCodeScanner;
 
-
-
 public class JavaScriptConfiguration extends SourceViewerConfiguration {
 
-	private XMLDoubleClickStrategy doubleClickStrategy;
-	private JSEditorCodeScanner textScanner;
-	private ColorManager colorManager;
+    private XMLDoubleClickStrategy doubleClickStrategy;
+    private JSEditorCodeScanner    textScanner;
+    private ColorManager           colorManager;
 
-	public JavaScriptConfiguration(ColorManager colorManager) {
-		this.colorManager = colorManager;
-	}
+    public JavaScriptConfiguration(ColorManager colorManager) {
+        this.colorManager = colorManager;
+    }
 
-	public String[] getConfiguredContentTypes(ISourceViewer sourceViewer) {
-		return new String[] { IDocument.DEFAULT_CONTENT_TYPE};
-	}
+    @Override
+    public IAutoEditStrategy[] getAutoEditStrategies(ISourceViewer sourceViewer, String contentType) {
+        return new IAutoEditStrategy[] { new JavaScriptAutoIndentStrategy() };
+    }
 
-	public ITextDoubleClickStrategy getDoubleClickStrategy(
-			ISourceViewer sourceViewer, String contentType) {
-		if (doubleClickStrategy == null)
-			doubleClickStrategy = new XMLDoubleClickStrategy(sourceViewer,contentType);
-		return doubleClickStrategy;
-	}
-	
-	protected JSEditorCodeScanner getJSScanner() {
-		if (textScanner == null) {
-			textScanner = new JSEditorCodeScanner(colorManager);
-			textScanner.setDefaultReturnToken(new Token(new TextAttribute(
-					colorManager.getColor(IColorConstants.DEFAULT))));
-		}
-		return textScanner;
-	}
+    public String[] getConfiguredContentTypes(ISourceViewer sourceViewer) {
+        return new String[] { IDocument.DEFAULT_CONTENT_TYPE };
+    }
 
-	public IPresentationReconciler getPresentationReconciler(
-			ISourceViewer sourceViewer) {
-		PresentationReconciler reconciler = new PresentationReconciler();
+    public ITextDoubleClickStrategy getDoubleClickStrategy(ISourceViewer sourceViewer, String contentType) {
+        if (doubleClickStrategy == null)
+            doubleClickStrategy = new XMLDoubleClickStrategy(sourceViewer, contentType);
+        return doubleClickStrategy;
+    }
 
-		DefaultDamagerRepairer dr = new DefaultDamagerRepairer(
-				getJSScanner());
-		reconciler.setDamager(dr, IDocument.DEFAULT_CONTENT_TYPE);
-		reconciler.setRepairer(dr, IDocument.DEFAULT_CONTENT_TYPE);
-		return reconciler;
-	}
+    protected JSEditorCodeScanner getJSScanner() {
+        if (textScanner == null) {
+            textScanner = new JSEditorCodeScanner(colorManager);
+            textScanner.setDefaultReturnToken(new Token(new TextAttribute(colorManager
+                    .getColor(IColorConstants.DEFAULT))));
+        }
+        return textScanner;
+    }
+
+    public IPresentationReconciler getPresentationReconciler(ISourceViewer sourceViewer) {
+        PresentationReconciler reconciler = new PresentationReconciler();
+
+        DefaultDamagerRepairer dr = new DefaultDamagerRepairer(getJSScanner());
+        reconciler.setDamager(dr, IDocument.DEFAULT_CONTENT_TYPE);
+        reconciler.setRepairer(dr, IDocument.DEFAULT_CONTENT_TYPE);
+        return reconciler;
+    }
 }
