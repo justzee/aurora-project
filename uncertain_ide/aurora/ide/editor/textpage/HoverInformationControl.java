7a01efa8f7cf48f8a167a7ddf3300b8a479cf9a6
jeschou.zsy <jeschou.zsy@gmail.com>
2011-10-20 08:51:12 +0000

    用于显示hover信息的information control
    可以以html的形式显示

---
+++ uncertain_ide/aurora/ide/editor/textpage/HoverInformationControl.java
@@ -0,0 +1,38 @@
+package aurora.ide.editor.textpage;
+
+import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.jface.internal.text.html.BrowserInformationControl;
+import org.eclipse.jface.text.DefaultInformationControl;
+import org.eclipse.jface.text.IInformationControl;
+import org.eclipse.jface.text.IInformationControlCreator;
+import org.eclipse.swt.widgets.Shell;
+
+@SuppressWarnings("restriction")
+public class HoverInformationControl extends BrowserInformationControl {
+    private String symbolicFontName;
+
+    public HoverInformationControl(Shell parent, String symbolicFontName, boolean resizable) {
+        super(parent, symbolicFontName, resizable);
+        this.symbolicFontName = symbolicFontName;
+    }
+
+    public HoverInformationControl(Shell parent, String symbolicFontName, String statusFieldText) {
+        super(parent, symbolicFontName, statusFieldText);
+        this.symbolicFontName = symbolicFontName;
+    }
+
+    public HoverInformationControl(Shell parent, String symbolicFontName, ToolBarManager toolBarManager) {
+        super(parent, symbolicFontName, toolBarManager);
+        this.symbolicFontName = symbolicFontName;
+    }
+
+    public IInformationControlCreator getInformationPresenterControlCreator() {
+        return new IInformationControlCreator() {
+            public IInformationControl createInformationControl(Shell parent) {
+                if (HoverInformationControl.isAvailable(parent))
+                    return new HoverInformationControl(parent, symbolicFontName, true);
+                return new DefaultInformationControl(parent);
+            }
+        };
+    }
+}
