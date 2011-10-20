192bf52b338d2e5d2f286a642d15bac0be1a6399
jeschou.zsy@gmail.com <jeschou.zsy@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2011-10-20 08:47:39 +0000

    添加新的校验: 验证标签是否可以出现在父标签下

--- trunk/uncertain_ide/aurora/ide/builder/processor/SxsdProcessor.java
+++ trunk/uncertain_ide/aurora/ide/builder/processor/SxsdProcessor.java
@@ -1,5 +1,7 @@
 package aurora.ide.builder.processor;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -7,20 +9,71 @@ import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.Region;
 
 import uncertain.composite.CompositeMap;
 import uncertain.schema.Attribute;
+import uncertain.schema.Element;
 import aurora.ide.builder.AuroraBuilder;
 import aurora.ide.builder.validator.AbstractValidator;
+import aurora.ide.editor.textpage.IColorConstants;
+import aurora.ide.helpers.CompositeMapUtil;
+import aurora.ide.helpers.LoadSchemaManager;
+import aurora.ide.search.core.Util;
 
 public class SxsdProcessor extends AbstractProcessor {
+    private static HashMap<String, List<String>> childListMap = new HashMap<String, List<String>>();
 
+    @SuppressWarnings("unchecked")
     @Override
     public void processMap(IFile file, CompositeMap map, IDocument doc) {
         if (map.getNamespaceURI() == null)
             return;
+        CompositeMap parent = map.getParent();
+        List<Element> childs = new ArrayList<Element>();
+        List<String> arrays = new ArrayList<String>(0);
+        if (parent != null) {
+            childs = CompositeMapUtil.getAvailableChildElements(parent);
+            if (childs == null)
+                childs = new ArrayList<Element>();
+            Element ele = LoadSchemaManager.getSchemaManager().getElement(parent);
+            if (ele != null) {
+                childs.addAll(ele.getAllArrays());
+            }
+        }
+        // System.out.println(map.getName());
+        boolean ok = false;
+        String mapName = map.getName();
+        // String parName = parent == null ? "根节点" : parent.getName();
+        for (Element ele : childs) {
+            // System.out.println(ele.getQName().getLocalName());
+            if (mapName.equalsIgnoreCase(ele.getQName().getLocalName())) {
+                ok = true;
+                break;
+            }
+        }
+        System.out.println();
+        if (!ok && parent != null) {
+            int line = map.getLocationNotNull().getStartLine();
+            if (line == 0)
+                line = 1;
+            IRegion region = null;
+            try {
+                System.out.println(doc.get(doc.getLineOffset(line - 1), doc.getLineLength(line - 1)));
+                region = Util.getDocumentRegion(doc.getLineOffset(line - 1), doc.getLineLength(line - 1),
+                        map.getRawName(), doc, IColorConstants.TAG_NAME);
+            } catch (BadLocationException e) {
+                region = new Region(0, 0);
+                e.printStackTrace();
+            }
+            System.out.println(region);
+            AuroraBuilder.addMarker(file, "Tag : " + mapName + " , 不应该出现在 " + parent.getName() + " 下", line, region,
+                    IMarker.SEVERITY_WARNING, AuroraBuilder.UNDEFINED_TAG);
+        }
+
         Set<String> nameSet = new HashSet<String>();
         List<Attribute> list = getAttributesInSchemaNotNull(map);
         for (Attribute a : list) {
