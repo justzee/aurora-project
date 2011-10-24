33d2db39ed60d38e4d7f3b9421040a61d7d20500
jeschou.zsy <jeschou.zsy@gmail.com>
2011-10-24 06:24:31 +0000

    修改addPages()方法 加入语句,可以在初始时,按照设置过的值打开指定的选项卡

--- uncertain_ide/aurora/ide/bm/editor/BussinessModelEditor.java
+++ uncertain_ide/aurora/ide/bm/editor/BussinessModelEditor.java
@@ -1,11 +1,11 @@
 package aurora.ide.bm.editor;
 
-
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 import org.eclipse.ui.PartInitException;
 
+import uncertain.composite.CompositeMap;
 import aurora.ide.editor.BaseCompositeMapEditor;
 import aurora.ide.editor.CompositeMapPage;
 import aurora.ide.helpers.ApplicationException;
@@ -14,90 +14,90 @@ import aurora.ide.helpers.CompositeMapLocatorParser;
 import aurora.ide.helpers.CompositeMapUtil;
 import aurora.ide.helpers.DialogUtil;
 import aurora.ide.helpers.LocaleMessage;
-
-
-import uncertain.composite.CompositeMap;
-
+import aurora.ide.preferencepages.CustomSettingPreferencePage;
 
 public class BussinessModelEditor extends BaseCompositeMapEditor {
 
-	protected BussinessModelPage mainFormPage;
-	private SQLExecutePage sqlPage = new SQLExecutePage(this);
-	private ViewSource viewSource = new ViewSource(this);
-	int SQLPageIndex;
-	private int viewSourceIndex;
-	
-	public CompositeMapPage initMainViewerPage() {
-		mainFormPage = new BussinessModelPage(this);
-		return mainFormPage;
-	}
+    protected BussinessModelPage mainFormPage;
+    private SQLExecutePage       sqlPage    = new SQLExecutePage(this);
+    private ViewSource           viewSource = new ViewSource(this);
+    int                          SQLPageIndex;
+    private int                  viewSourceIndex;
 
-	protected void addPages() {
-		try {
-			super.addPages();
-			SQLPageIndex = addPage(sqlPage);
-			viewSourceIndex = addPage(viewSource, getEditorInput());
-			this.setPageText(viewSourceIndex, LocaleMessage.getString("view.source"));
-		} catch (PartInitException e) {
-			DialogUtil.showExceptionMessageBox(e);
-		}
-	}
+    public CompositeMapPage initMainViewerPage() {
+        mainFormPage = new BussinessModelPage(this);
+        return mainFormPage;
+    }
 
-	public void editorDirtyStateChanged() {
-		super.editorDirtyStateChanged();
-		sqlPage.setModify(true);
-	}
+    protected void addPages() {
+        try {
+            super.addPages();
+            SQLPageIndex = addPage(sqlPage);
+            viewSourceIndex = addPage(viewSource, getEditorInput());
+            this.setPageText(viewSourceIndex, LocaleMessage.getString("view.source"));
+            setActivePage(CustomSettingPreferencePage.getBMEditorInitPageIndex());
+        } catch (PartInitException e) {
+            DialogUtil.showExceptionMessageBox(e);
+        }
+    }
 
-	protected void pageChange(int newPageIndex) {
-		int currentPage = getCurrentPage();
-		super.pageChange(newPageIndex);
-		if (newPageIndex == SQLPageIndex) {
-			try {
-				sqlPage.refresh(CompositeMapUtil.getFullContent(mainFormPage.getData()));
-			} catch (ApplicationException e) {
-				DialogUtil.showExceptionMessageBox(e);
-			}
-		} else if (newPageIndex == viewSourceIndex) {
-			viewSource.refresh();
-		} else if (currentPage == mainViewerIndex && newPageIndex == textPageIndex && mainViewerPage.isFormContendCreated()) {
-			try {
-				locateTextPage();
-			} catch (ApplicationException e) {
-				DialogUtil.showExceptionMessageBox(e);
-			}
-		} else if (currentPage == textPageIndex && newPageIndex == mainViewerIndex&&textPage.checkContentFormat()) {
-			locateMainPage();
-		}
-	}
+    public void editorDirtyStateChanged() {
+        super.editorDirtyStateChanged();
+        sqlPage.setModify(true);
+    }
 
-	private void locateMainPage() {
-		CompositeMapLocatorParser parser = new CompositeMapLocatorParser();
+    protected void pageChange(int newPageIndex) {
+        int currentPage = getCurrentPage();
+        super.pageChange(newPageIndex);
+        if (newPageIndex == SQLPageIndex) {
+            try {
+                sqlPage.refresh(CompositeMapUtil.getFullContent(mainFormPage.getData()));
+            } catch (ApplicationException e) {
+                DialogUtil.showExceptionMessageBox(e);
+            }
+        } else if (newPageIndex == viewSourceIndex) {
+            viewSource.refresh();
+        } else if (currentPage == mainViewerIndex && newPageIndex == textPageIndex
+                && mainViewerPage.isFormContendCreated()) {
+            try {
+                locateTextPage();
+            } catch (ApplicationException e) {
+                DialogUtil.showExceptionMessageBox(e);
+            }
+        } else if (currentPage == textPageIndex && newPageIndex == mainViewerIndex && textPage.checkContentFormat()) {
+            locateMainPage();
+        }
+    }
 
-		try {
-			InputStream content = new ByteArrayInputStream(textPage.getContent().getBytes("UTF-8"));
-			CompositeMap cm = parser.getCompositeMapFromLine(content, textPage.getCursorLine());
-			if (cm != null) {
-				while (cm.getParent() != null) {
-					CompositeMap parent = cm.getParent();
-					if (AuroraConstant.ModelQN.equals(parent.getQName())) {
-						mainFormPage.setSelectionTab(cm.getName());
-					}
-					cm = parent;
-				}
-			}
-		} catch (Exception e) {
-			DialogUtil.showExceptionMessageBox(e);
-		}
+    private void locateMainPage() {
+        CompositeMapLocatorParser parser = new CompositeMapLocatorParser();
 
-	}
+        try {
+            InputStream content = new ByteArrayInputStream(textPage.getContent().getBytes("UTF-8"));
+            CompositeMap cm = parser.getCompositeMapFromLine(content, textPage.getCursorLine());
+            if (cm != null) {
+                while (cm.getParent() != null) {
+                    CompositeMap parent = cm.getParent();
+                    if (AuroraConstant.ModelQN.equals(parent.getQName())) {
+                        mainFormPage.setSelectionTab(cm.getName());
+                    }
+                    cm = parent;
+                }
+            }
+        } catch (Exception e) {
+            DialogUtil.showExceptionMessageBox(e);
+        }
 
-	private void locateTextPage() throws ApplicationException {
-		CompositeMap selection = mainFormPage.getSelectionTab();
-		if (selection == null)
-			return;
-		int line = 0;
-		line = CompositeMapUtil.locateNode(CompositeMapUtil.getFullContent(mainFormPage.getData()), selection);
-		int offset = textPage.getOffsetFromLine(line);
-		textPage.setHighlightRange(offset, 10, true);
+    }
 
-	}}
+    private void locateTextPage() throws ApplicationException {
+        CompositeMap selection = mainFormPage.getSelectionTab();
+        if (selection == null)
+            return;
+        int line = 0;
+        line = CompositeMapUtil.locateNode(CompositeMapUtil.getFullContent(mainFormPage.getData()), selection);
+        int offset = textPage.getOffsetFromLine(line);
+        textPage.setHighlightRange(offset, 10, true);
+
+    }
+}
--- uncertain_ide/aurora/ide/screen/editor/ServiceEditor.java
+++ uncertain_ide/aurora/ide/screen/editor/ServiceEditor.java
@@ -1,40 +1,42 @@
 package aurora.ide.screen.editor;
 
-
 import org.eclipse.ui.PartInitException;
 
 import aurora.ide.editor.CompositeMapTreeEditor;
 import aurora.ide.editor.CompositeMapTreePage;
 import aurora.ide.helpers.DialogUtil;
+import aurora.ide.preferencepages.CustomSettingPreferencePage;
 
+public class ServiceEditor extends CompositeMapTreeEditor {
 
-public class ServiceEditor extends CompositeMapTreeEditor{
+    protected int       browserPageIndex;
+    private BrowserPage browsePage;
 
+    public CompositeMapTreePage initTreePage() {
+        ServiceTreePage treePage = new ServiceTreePage(this);
+        return treePage;
+    }
 
-	protected int browserPageIndex;
-	private BrowserPage browsePage;
-	public CompositeMapTreePage initTreePage() {
-		ServiceTreePage treePage = new ServiceTreePage(this);
-		return treePage;
-	}
-	
-	protected void addPages() {
-		browsePage = new BrowserPage(this);
-		try {
-			super.addPages();
-			browserPageIndex=addPage(browsePage);
-		} catch (PartInitException e) {
-			DialogUtil.showExceptionMessageBox(e);
-		}
-	}
-	protected void pageChange(int newPageIndex){
-		if(newPageIndex == browserPageIndex){
-			browsePage.refresh();
-		}
-		super.pageChange(newPageIndex);
-	}
-	public void editorDirtyStateChanged() {
-		super.editorDirtyStateChanged();
-		browsePage.setModify(true);
-	}
+    protected void addPages() {
+        browsePage = new BrowserPage(this);
+        try {
+            super.addPages();
+            browserPageIndex = addPage(browsePage);
+            setActivePage(CustomSettingPreferencePage.getScreenEditorInitPageIndex());
+        } catch (PartInitException e) {
+            DialogUtil.showExceptionMessageBox(e);
+        }
+    }
+
+    protected void pageChange(int newPageIndex) {
+        if (newPageIndex == browserPageIndex) {
+            browsePage.refresh();
+        }
+        super.pageChange(newPageIndex);
+    }
+
+    public void editorDirtyStateChanged() {
+        super.editorDirtyStateChanged();
+        browsePage.setModify(true);
+    }
 }
