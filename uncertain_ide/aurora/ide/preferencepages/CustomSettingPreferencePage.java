4ab6c48e31221996fa682a891ef4dffb8942af73
jeschou.zsy <jeschou.zsy@gmail.com>
2011-10-24 06:26:04 +0000

    偏好设置选项卡 目前可以设置screen,bm编辑器的初始选项卡

---
+++ uncertain_ide/aurora/ide/preferencepages/CustomSettingPreferencePage.java
@@ -0,0 +1,52 @@
+package aurora.ide.preferencepages;
+
+import org.eclipse.jface.preference.ComboFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+import aurora.ide.AuroraPlugin;
+import aurora.ide.helpers.LocaleMessage;
+
+public class CustomSettingPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+    protected final static String screenEditorInitPageId = "screenEditorInitPageId";
+    protected final static String bmEditorInitPageId     = "bmEditorInitPageId";
+    private static String[][]     screenPage             = { { LocaleMessage.getString("screen.file"), "0" },
+            { LocaleMessage.getString("source.file"), "1" }, { LocaleMessage.getString("preview"), "2" } };
+    private static String[][]     bmPage                 = { { LocaleMessage.getString("bussiness.model.file"), "0" },
+            { LocaleMessage.getString("source.file"), "1" }, { LocaleMessage.getString("auto.sql.test"), "2" },
+            { LocaleMessage.getString("view.source"), "3" } };
+
+    public CustomSettingPreferencePage() {
+        super(FieldEditorPreferencePage.GRID);
+        IPreferenceStore store = AuroraPlugin.getDefault().getPreferenceStore();
+        setPreferenceStore(store);
+    }
+
+    public void init(IWorkbench workbench) {
+
+    }
+
+    @Override
+    protected void createFieldEditors() {
+        addField(new ComboFieldEditor(screenEditorInitPageId, "Screen编辑器初始选项卡", screenPage, getFieldEditorParent()));
+        addField(new ComboFieldEditor(bmEditorInitPageId, "BM编辑器初始选项卡", bmPage, getFieldEditorParent()));
+    }
+
+    public static int getScreenEditorInitPageIndex() {
+        try {
+            return Integer.parseInt(AuroraPlugin.getDefault().getPreferenceStore().getString(screenEditorInitPageId));
+        } catch (Exception e) {
+        }
+        return 0;
+    }
+
+    public static int getBMEditorInitPageIndex() {
+        try {
+            return Integer.parseInt(AuroraPlugin.getDefault().getPreferenceStore().getString(bmEditorInitPageId));
+        } catch (Exception e) {
+        }
+        return 0;
+    }
+}
