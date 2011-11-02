1e05d4c5ecab1fef00d5df5442f4239222b4a93e
bobbie.zou <bobbie.zou@gmail.com>
2011-11-02 05:12:51 +0000

    NTLM域验证

---
+++ org/lwap/plugin/ntlm/NtlmConfig.java
@@ -0,0 +1,81 @@
+package org.lwap.plugin.ntlm;
+
+import jcifs.Config;
+import uncertain.composite.CompositeMap;
+import uncertain.ocm.IObjectRegistry;
+
+public class NtlmConfig {
+	String procedure;
+	String checksql;
+	String checksession;
+	String checksql_result;
+	String checksession_result;
+	CompositeMap domainInstances;
+	IObjectRegistry mObjectRegistry;
+	
+	public NtlmConfig(IObjectRegistry registry) {
+		mObjectRegistry=registry;
+		Config.setProperty("jcifs.smb.client.soTimeout", "1800000");
+		Config.setProperty("jcifs.netbios.cachePolicy", "1200");
+		Config.setProperty("jcifs.smb.lmCompatibility", "0");
+		Config.setProperty("jcifs.smb.client.useExtendedSecurity", "false");
+	}
+	
+	public void onInitialize() {
+		mObjectRegistry.registerInstance(NtlmConfig.class, this);		
+	}
+	
+	public String getChecksql() {
+		return checksql;
+	}
+
+	public void setChecksql(String checksql) {
+		this.checksql = checksql;
+	}
+
+	public String getChecksession() {
+		return checksession;
+	}
+
+	public void setChecksession(String checksession) {
+		this.checksession = checksession;
+	}
+
+	public String getChecksql_result() {
+		return checksql_result;
+	}
+
+	public void setChecksql_result(String checksql_result) {
+		this.checksql_result = checksql_result;
+	}
+
+	public String getChecksession_result() {
+		return checksession_result;
+	}
+
+	public void setChecksession_result(String checksession_result) {
+		this.checksession_result = checksession_result;
+	}
+
+	public String getProcedure() {
+		return procedure;
+	}
+
+	public void setProcedure(String procedure) {
+		this.procedure = procedure;
+	}
+	
+	public DomainInstance getDomainInstance(String domain) {
+		return (DomainInstance) domainInstances.get(domain);
+	}	
+
+	public void addInstances(DomainInstance[] domainInstance) {
+		domainInstances = new CompositeMap();
+		int l = domainInstance.length;
+		DomainInstance intance = null;
+		for (int i = 0; i < l; i++) {
+			intance = domainInstance[i];
+			domainInstances.put(intance.getDomain(), intance);
+		}
+	}
+}
---
+++ org/lwap/plugin/ntlm/NtlmLogin.java
@@ -0,0 +1,88 @@
+package org.lwap.plugin.ntlm;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jcifs.smb.NtlmPasswordAuthentication;
+
+import org.lwap.application.event.AbstractServiceHandle;
+import org.lwap.application.event.SessionController;
+import org.lwap.controller.MainService;
+
+import uncertain.composite.CompositeMap;
+import uncertain.composite.TextParser;
+import uncertain.event.EventModel;
+import uncertain.logging.ILogger;
+import uncertain.logging.LoggingContext;
+import uncertain.ocm.IObjectRegistry;
+
+public class NtlmLogin extends AbstractServiceHandle {
+	IObjectRegistry mObjectRegistry;
+	NtlmConfig ntlmConfig;
+	ILogger mLogger;
+
+	public NtlmLogin(IObjectRegistry registry) {
+		mObjectRegistry = registry;
+		ntlmConfig = (NtlmConfig) mObjectRegistry.getInstanceOfType(NtlmConfig.class);
+		mLogger = LoggingContext.getLogger("org.lwap.plugin.ntlm",mObjectRegistry);
+	}
+
+	public int handleEvent(int sequence, CompositeMap context,
+			Object[] parameters) throws Exception {	
+		
+		MainService service = MainService.getServiceInstance(context);
+		HttpServletRequest httpRequest = service.getRequest();
+		HttpServletResponse httpResponse = service.getResponse();
+		
+		if (!checkSession(service)) {
+			String username;
+			CompositeMap parameter=service.getParameters();
+			CompositeMap model=service.getModel();		
+		
+			String serviceName = httpRequest.getRequestURI().substring(
+					httpRequest.getContextPath().length() + 1);
+			
+			parameter.putString("service_name", serviceName);
+			service.databaseAccess(ntlmConfig.getChecksql(), parameter, model);			
+			mLogger.info("context:"+context.toXML());
+			
+			if ("0".equalsIgnoreCase(TextParser.parse(ntlmConfig.getChecksql_result(), context))){
+				mLogger.info(serviceName+" is not login required");
+				//如果不需要权限验证跳过域验证
+				return EventModel.HANDLE_NORMAL;
+			}
+			NtlmPasswordAuthentication ntlm = null;
+			try {
+				if ((ntlm = new NtlmAuthenticator(ntlmConfig).authenticate(httpRequest,
+						httpResponse)) == null) {
+					SessionController state = SessionController
+							.createSessionController(context);
+					state.setContinueFlag(false);
+					return EventModel.HANDLE_STOP;
+				}
+			} catch (Exception e) {
+				//域验证不通过，跳入普通处理方式
+				return EventModel.HANDLE_NORMAL;
+			}
+			username=ntlm.getUsername();
+			parameter.put("user_name", username.toUpperCase());
+			mLogger.info("username:"+username);
+			mLogger.info("excute procedure "+ntlmConfig.getProcedure());
+			service.databaseAccess(ntlmConfig.getProcedure(), parameter, model);			
+			mLogger.info("doLogin context:"+context.toXML());
+		}
+		
+		return EventModel.HANDLE_NORMAL;
+	}
+
+	boolean checkSession(MainService service) throws ServletException {
+		CompositeMap parameter = service.getParameters();
+		CompositeMap model = service.getModel();
+		parameter.put("encrypted_session_id", service.getServiceContext()
+				.getObject("/cookie/@JSID/@value"));
+		service.databaseAccess(ntlmConfig.getChecksession(), parameter, model);
+		return ((CompositeMap) model.getObject(ntlmConfig
+				.getChecksession_result())).getChilds() == null ? false : true;
+	}
+}
