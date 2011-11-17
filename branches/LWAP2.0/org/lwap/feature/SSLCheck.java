b81d01160e6f205e8afd377042769a3572271190
njq.niu@gmail.com <njq.niu@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2011-11-17 02:30:11 +0000

    为玉柴开发. 将证书和系统用户绑定

--- branches/LWAP2.0/org/lwap/feature/SSLCheck.java
+++ branches/LWAP2.0/org/lwap/feature/SSLCheck.java
@@ -1,12 +1,15 @@
 package org.lwap.feature;
 
+import java.sql.Connection;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
 
 import org.lwap.application.event.AbstractServiceHandle;
 import org.lwap.application.event.SessionController;
@@ -15,24 +18,71 @@ import org.lwap.controller.MainService;
 import uncertain.composite.CompositeMap;
 import uncertain.event.EventModel;
 
+import aurora.database.FetchDescriptor;
+import aurora.database.service.DatabaseServiceFactory;
+import aurora.database.service.RawSqlService;
+import aurora.database.service.SqlServiceContext;
+
 public class SSLCheck extends AbstractServiceHandle {
+	
+	private static final String CUX_USER_CERTS_SERVICE = "sys.cux_user_certs_service";
 	
 	private static final String URL_HTTPS = "https";
 //	private static final int DEFAULT_HTTPS_PORT = 8443;
 	
+	private DatabaseServiceFactory mSvcFactory;
+	
+	private DataSource dataSource;
+	
 	private SSLCheckConfig config;
 	
-	public SSLCheck(SSLCheckConfig config){
+	public SSLCheck(SSLCheckConfig config,DatabaseServiceFactory factory,DataSource ds){
 		if(config == null) throw new RuntimeException("SSLCheck config undefined");
 		this.config = config;
+		mSvcFactory = factory;
+		dataSource = ds;
+		
 	}
 
 	public int handleEvent(int sequence, CompositeMap context,Object[] parameters) throws Exception {
 		MainService svc = MainService.getServiceInstance(context);
 		HttpServletRequest request = svc.getRequest();
 		String service_name = svc.getServiceName();
-		List services = this.config.getAllServices();		
-		if (!request.isSecure() && services.contains(service_name.toLowerCase())) {
+		List services = this.config.getAllServices();
+		if(request.isSecure()){
+			java.security.cert.X509Certificate[] certChain= (java.security.cert.X509Certificate[])request.getAttribute("javax.servlet.request.X509Certificate"); 
+			if (null!=certChain){
+				java.security.cert.X509Certificate x509Cert = (java.security.cert.X509Certificate)certChain[0];
+				String serialNumber = x509Cert.getSerialNumber().toString(16);
+				Connection conn = dataSource.getConnection();
+				try{
+					SqlServiceContext sqlServiceContext = SqlServiceContext.createSqlServiceContext(conn);
+					sqlServiceContext.getParameter().put("userId", context.getObject("/session/@user_id"));
+					RawSqlService sqlService = mSvcFactory.getSqlService(CUX_USER_CERTS_SERVICE);
+					CompositeMap resultMap = sqlService.queryAsMap(sqlServiceContext, FetchDescriptor.getDefaultInstance());
+					List childs = new ArrayList();
+					if (resultMap != null && resultMap.getChilds() != null){
+						childs = resultMap.getChilds();
+						CompositeMap scol = (CompositeMap)childs.get(0);
+						String serialnum = scol.getString("CERTS_SERIAL_NUMBER");
+						if(!serialNumber.equals(serialnum)){
+							SessionController sc = SessionController.createSessionController(context);
+							sc.setContinueFlag(false);
+							sc.setDispatchUrl("https://"+request.getServerName()+":"+config.getHttpsPort()+request.getContextPath()+"/user_certs_verify.htm");
+							return EventModel.HANDLE_STOP;
+						}
+					}else{
+						SessionController sc = SessionController.createSessionController(context);
+						sc.setContinueFlag(false);
+						sc.setDispatchUrl("https://"+request.getServerName()+":"+config.getHttpsPort()+request.getContextPath()+"/user_certs_verify.htm");
+						return EventModel.HANDLE_STOP;
+					}
+				}finally{
+					if (conn != null)
+						conn.close();
+				}
+			}else{return EventModel.HANDLE_STOP;}
+		}else if(!request.isSecure() && services.contains(service_name.toLowerCase())) {
 			String despachUrl = buildSSLUrl(request);
 			SessionController sc = SessionController.createSessionController(context);
 			sc.setContinueFlag(false);
