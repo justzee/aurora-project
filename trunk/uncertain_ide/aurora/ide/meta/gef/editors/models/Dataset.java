4cf2d9092e28eede23d53508a1c926d0f7a5aea8
rufus.sly@gmail.com <rufus.sly@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-01-06 05:49:48 +0000

    ide

--- trunk/uncertain_ide/aurora/ide/meta/gef/editors/models/Dataset.java
+++ trunk/uncertain_ide/aurora/ide/meta/gef/editors/models/Dataset.java
@@ -3,6 +3,11 @@ package aurora.ide.meta.gef.editors.models;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
+import org.eclipse.ui.views.properties.IPropertyDescriptor;
+
+import aurora.ide.meta.gef.editors.property.BooleanPropertyDescriptor;
+import aurora.ide.meta.gef.editors.property.StringPropertyDescriptor;
+
 public class Dataset extends AuroraComponent {
 	/**
 	 * 
@@ -19,11 +24,47 @@ public class Dataset extends AuroraComponent {
 	private boolean isUse4Query;
 	// 是否使用父的BM
 	private boolean isUseParentBM = true;
+	
+	
+	public static final String AUTO_QUERY ="autoQuery";
+	public static final String MODEL ="model";
+	public static final String ID ="id";
+	
+	
+
+	private static final IPropertyDescriptor[] pds = new IPropertyDescriptor[] {
+			new StringPropertyDescriptor(AUTO_QUERY, "autoQuery"),
+			new StringPropertyDescriptor(MODEL, "model"),
+			new StringPropertyDescriptor(ID, "id")};
 
 	public Dataset() {
 		// 暂时不显示
 		// this.setSize(new Dimension(50, 20));
 		this.setType("dataSet");
+	}
+
+	@Override
+	public IPropertyDescriptor[] getPropertyDescriptors() {
+		
+		return pds;
+	}
+
+	@Override
+	public Object getPropertyValue(Object propName) {
+		if(AUTO_QUERY.equals(propName)){
+			return this.isAutoQuery();
+		}
+		if(MODEL.equals(propName)){
+			return this.getModelPKG();
+		}
+		if(ID.equals(propName)){
+			return this.getId();
+		}
+		return null;
+	}
+
+	private Object getModelPKG() {
+		return "a.b.c";
 	}
 
 	public String getId() {
--- trunk/uncertain_ide/aurora/ide/meta/gef/editors/models/ResultDataSet.java
+++ trunk/uncertain_ide/aurora/ide/meta/gef/editors/models/ResultDataSet.java
@@ -1,5 +1,9 @@
 package aurora.ide.meta.gef.editors.models;
 
+import org.eclipse.ui.views.properties.IPropertyDescriptor;
+
+import aurora.ide.meta.gef.editors.property.StringPropertyDescriptor;
+
 public class ResultDataSet extends Dataset {
 
 	/**
@@ -14,11 +18,54 @@ public class ResultDataSet extends Dataset {
 	private boolean selectable = false;
 
 	// private String selectionModel="multiple"/"single" ;
-	private String selectionModel = Grid.SELECT_NONE;
+	private String selectionMode = Grid.SELECT_NONE;
+
+	public static final String SELECTION_MODE = "selectionMode";
+	public static final String PAGE_SIZE = "pageSize";
+	public static final String SELECTABLE = "selectable";
+	public static final String QUER_DATASET = "queryDataSet";
+
+	private static final IPropertyDescriptor[] pds = new IPropertyDescriptor[] {
+			new StringPropertyDescriptor(AUTO_QUERY, "autoQuery"),
+			new StringPropertyDescriptor(MODEL, "model"),
+			new StringPropertyDescriptor(ID, "id"),
+			new StringPropertyDescriptor(SELECTION_MODE, "selectionMode"),
+			new StringPropertyDescriptor(PAGE_SIZE, "pageSize"),
+			new StringPropertyDescriptor(SELECTABLE, "selectable"),
+			new StringPropertyDescriptor(QUER_DATASET, "queryDataSet") };
 
 	public ResultDataSet() {
 		this.setUse4Query(false);
 		this.setPageSize(DEFAULT_PAGE_SIZE);
+	}
+
+	public IPropertyDescriptor[] getPropertyDescriptors() {
+
+		return pds;
+	}
+
+	@Override
+	public Object getPropertyValue(Object propName) {
+		if (SELECTION_MODE.equals(propName)) {
+			return this.getSelectionMode();
+		}
+
+		if (PAGE_SIZE.equals(propName)) {
+			return this.getPageSize();
+		}
+
+		if (SELECTABLE.equals(propName)) {
+			return this.isSelectable();
+		}
+
+		if (QUER_DATASET.equals(propName)) {
+			return this.getQueryDataset();
+		}
+		return super.getPropertyValue(propName);
+	}
+
+	private String getQueryDataset() {
+		return ((Container) this.getQueryContainer()).getDataset().getId();
 	}
 
 	public AuroraComponent getQueryContainer() {
@@ -45,12 +92,12 @@ public class ResultDataSet extends Dataset {
 		this.selectable = selectable;
 	}
 
-	public String getSelectionModel() {
-		return selectionModel;
+	public String getSelectionMode() {
+		return selectionMode;
 	}
 
-	public void setSelectionModel(String selectionModel) {
-		this.selectionModel = selectionModel;
+	public void setSelectionMode(String selectionMode) {
+		this.selectionMode = selectionMode;
 	}
 
 }
