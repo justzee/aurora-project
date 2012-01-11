aa1af934efbda3f7ed5cd2e0d92522bb78134ae5
wxjivm <wxjivm@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-01-11 02:47:59 +0000

    自定义扩展点，增加新建meta文件的模板向导

---
+++ trunk/uncertain_ide/aurora/ide/meta/gef/extension/ExtensionBean.java
@@ -0,0 +1,59 @@
+package aurora.ide.meta.gef.extension;
+
+import aurora.ide.meta.gef.editors.wizard.INewTemplateWizard;
+
+public class ExtensionBean {
+	private String id;
+	private String name;
+	private String description;
+	private String thumbnail;
+	private INewTemplateWizard wizard;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getThumbnail() {
+		return thumbnail;
+	}
+
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+
+	public INewTemplateWizard getWizard() {
+		return wizard;
+	}
+
+	public void setWizard(INewTemplateWizard wizard) {
+		this.wizard = wizard;
+	}
+
+	@Override
+	public boolean equals(Object anObject) {
+		if (anObject instanceof ExtensionBean) {
+			return ((ExtensionBean) anObject).getId().equals(id) && ((ExtensionBean) anObject).getName().equals(name);
+		}
+		return false;
+	}
+}
