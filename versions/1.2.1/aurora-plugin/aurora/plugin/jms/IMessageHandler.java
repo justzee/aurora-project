8dbf7d73af6a9e84b7dd02ddeec2b86796102f6b
lahmygc@gmail.com <lahmygc@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-03-17 05:15:47 +0000

    ant测试

--- versions/1.2.1/aurora-plugin/aurora/plugin/jms/IMessageHandler.java
+++
@@ -1,8 +0,0 @@
-package aurora.plugin.jms;
-
-import javax.jms.MessageListener;
-
-public interface IMessageHandler extends MessageListener{
-	public String getName();
-	public void setName(String name) ;
-}
