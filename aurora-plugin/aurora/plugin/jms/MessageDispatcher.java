ca08d302790ef9f3a24be9e694ae0c75c90455ee
linjinxiao <linjinxiao@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-05-21 06:36:52 +0000

    检查jms client是否启动成功

--- aurora-plugin/aurora/plugin/jms/MessageDispatcher.java
+++ aurora-plugin/aurora/plugin/jms/MessageDispatcher.java
@@ -20,24 +20,29 @@ import uncertain.ocm.AbstractLocatableObject;
 import uncertain.ocm.IObjectRegistry;
 import aurora.application.features.msg.IMessage;
 import aurora.application.features.msg.IMessageDispatcher;
+import aurora.application.features.msg.IMessageStub;
 
 public class MessageDispatcher extends AbstractLocatableObject implements IMessageDispatcher {
 
 	private IObjectRegistry mRegistry;
-	
+
 	public MessageDispatcher(IObjectRegistry registry) {
 		this.mRegistry = registry;
 	}
-	public void send(String topic,IMessage message, CompositeMap context) throws Exception {
+
+	public void send(String topic, IMessage message, CompositeMap context) throws Exception {
 		if (topic == null)
 			BuiltinExceptionFactory.createAttributeMissing(this, "topic");
-		ILogger logger = LoggingContext.getLogger(context,
-				JMSUtil.PLUGIN);
-		ConnectionFactory connectionFactory = (ConnectionFactory) mRegistry
-				.getInstanceOfType(ConnectionFactory.class);
+		ILogger logger = LoggingContext.getLogger(context, JMSUtil.PLUGIN);
+		IMessageStub messageStub = (IMessageStub) mRegistry.getInstanceOfType(IMessageStub.class);
+		if (messageStub == null)
+			throw BuiltinExceptionFactory.createInstanceNotFoundException(null, IMessageStub.class, this.getClass().getCanonicalName());
+		if (!messageStub.isStarted()) {
+			throw new IllegalStateException("MessageStub is not started, please check the configuration.");
+		}
+		ConnectionFactory connectionFactory = (ConnectionFactory) mRegistry.getInstanceOfType(ConnectionFactory.class);
 		if (connectionFactory == null) {
-			throw BuiltinExceptionFactory.createInstanceNotFoundException(this,
-					ConnectionFactory.class);
+			throw BuiltinExceptionFactory.createInstanceNotFoundException(this, ConnectionFactory.class);
 		}
 		Connection connection = null;
 		Session session = null;
@@ -45,8 +50,7 @@ public class MessageDispatcher extends AbstractLocatableObject implements IMessa
 		try {
 			connection = connectionFactory.createConnection();
 			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			logger.log(Level.CONFIG, "create createTopic {0}",
-					new Object[] { topic });
+			logger.log(Level.CONFIG, "create createTopic {0}", new Object[] { topic });
 			Topic jmsTopic = session.createTopic(topic);
 			messageProducer = session.createProducer(jmsTopic);
 			logger.log(Level.CONFIG, "start producer connection");
@@ -54,23 +58,22 @@ public class MessageDispatcher extends AbstractLocatableObject implements IMessa
 			logger.log(Level.CONFIG, "start producer successfull!");
 			String parsedText = TextParser.parse(message.getText(), context);
 			Message textMessage = session.createTextMessage(parsedText);
-			
+
 			CompositeMap properties = message.getProperties();
-			if(properties != null && !properties.isEmpty()){
+			if (properties != null && !properties.isEmpty()) {
 				Set<Entry<Object, Object>> set = properties.entrySet();
 				for (Entry<Object, Object> en : set) {
-					textMessage.setObjectProperty(en.getKey().toString(), TextParser.parse(en
-							.getValue().toString(), context));
+					textMessage.setObjectProperty(en.getKey().toString(), TextParser.parse(en.getValue().toString(), context));
 				}
 			}
 			messageProducer.send(textMessage);
-			logger.log(Level.CONFIG, "Message:{0} sent",new Object[] { parsedText });
+			logger.log(Level.CONFIG, "Message:{0} sent", new Object[] { parsedText });
 		} finally {
 			JMSUtil.freeMessageProducer(messageProducer);
 			JMSUtil.freeJMSSession(session);
 			JMSUtil.freeJMSConnection(connection);
 		}
-		
+
 	}
 
 }
