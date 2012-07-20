7d6ceea24f25c07e9664e41d8dfead97555e2865
bobbie.zou@gmail.com <bobbie.zou@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-07-20 08:31:04 +0000

    支持model-query标签嵌套，既model-query不是init-procedure标签的child

--- trunk/aurora-plugin/aurora/plugin/export/ModelOutput.java
+++ trunk/aurora-plugin/aurora/plugin/export/ModelOutput.java
@@ -25,20 +25,24 @@ public class ModelOutput {
 	public final String KEY_WIDTH = "width";
 	public final String KEY_GENERATE_STATE = "_generate_state";
 	public final String KEY_FORMAT = "_format";
-	public final String KEY_EXCEL="xls";
+	public final String KEY_EXCEL = "xls";
 
 	IObjectRegistry mObjectRegistry;
+	
+	int modelQueryTagNum;
 
 	public ModelOutput(IObjectRegistry registry) {
 		mObjectRegistry = registry;
 	}
 
 	public int preInvokeService(ServiceContext context) throws Exception {
-		CompositeMap parameters=context.getParameter();
+		CompositeMap parameters = context.getParameter();
 		if (!parameters.getBoolean(KEY_GENERATE_STATE, false))
 			return EventModel.HANDLE_NORMAL;
-		ILogger mLogger = LoggingContext.getLogger("aurora.plugin.export",mObjectRegistry);
-		ServiceInstance svc = ServiceInstance.getInstance(context.getObjectContext());
+		ILogger mLogger = LoggingContext.getLogger("aurora.plugin.export",
+				mObjectRegistry);
+		ServiceInstance svc = ServiceInstance.getInstance(context
+				.getObjectContext());
 
 		// 修改fetchall为ture
 		CompositeMap config = svc.getServiceConfigData().getChild(
@@ -47,31 +51,59 @@ public class ModelOutput {
 			mLogger.log(Level.SEVERE, "init-procedure tag must be defined");
 			throw new ServletException("init-procedure tag must be defined");
 		}
-		String return_path = (String) svc.getServiceConfigData().getObject(ServiceOutputConfig.KEY_SERVICE_OUTPUT + "/@"
-				+ ServiceOutputConfig.KEY_OUTPUT);
-		String rootpath;
-		Iterator iterator = config.getChildIterator();
-		CompositeMap modelQueryMap;
+
+		String return_path = (String) svc.getServiceConfigData().getObject(
+				ServiceOutputConfig.KEY_SERVICE_OUTPUT + "/@"
+						+ ServiceOutputConfig.KEY_OUTPUT);
+		if (return_path == null) {
+			mLogger.log(Level.SEVERE, "service-output tag must be defined");
+			throw new ServletException("service-output tag must be defined");
+		} else {
+			if (!return_path.startsWith("/"))
+				return_path = "/" + return_path;
+		}
+		modelQueryTagNum=0;
+		createConsumerTag(config, return_path, parameters);
+		if(modelQueryTagNum==0){
+			mLogger.log(Level.SEVERE, "The path '"+return_path+"' can't find model-query tag");
+			throw new ServletException("The path '"+return_path+"' can't find model-query tag");
+		}
+		return EventModel.HANDLE_NORMAL;
+	}
+
+	boolean createConsumerTag(CompositeMap actionConfig, String return_path,
+			CompositeMap parameters) {
+		if(modelQueryTagNum!=0)return true;
+		Iterator iterator = actionConfig.getChildIterator();
 		if (iterator != null) {
 			while (iterator.hasNext()) {
-				modelQueryMap = (CompositeMap) iterator.next();
-				rootpath=modelQueryMap.getString("rootpath");				
-				if(rootpath!=null){
-					if(!rootpath.startsWith("/"))
-						rootpath="/model/"+rootpath;
-					if(!return_path.startsWith("/"))
-						return_path="/"+return_path;
-					
-					if ("model-query".equals(modelQueryMap.getName())&&return_path.equalsIgnoreCase(rootpath)){
-						modelQueryMap.putBoolean(DataSetConfig.PROPERTITY_FETCHALL,true);
-						if(KEY_EXCEL.equals(parameters.getString(KEY_FORMAT)))
-							modelQueryMap.createChildByTag("consumer").createChildByTag("output-excel").setNameSpaceURI("http://www.aurora-framework.org/application");
+				if (createConsumerTag((CompositeMap) iterator.next(),
+						return_path, parameters)) {					
+					break;
+				}
+			}
+		} else {
+			String rootpath = actionConfig.getString("rootpath");
+			if (rootpath != null) {
+				if (!rootpath.startsWith("/"))
+					rootpath = "/model/" + rootpath;
+				if ("model-query".equals(actionConfig.getName())
+						&& return_path.equalsIgnoreCase(rootpath)) {
+					actionConfig.putBoolean(DataSetConfig.PROPERTITY_FETCHALL,
+							true);
+					if (KEY_EXCEL.equals(parameters.getString(KEY_FORMAT))) {
+						actionConfig
+								.createChildByTag("consumer")
+								.createChildByTag("output-excel")
+								.setNameSpaceURI(
+										"http://www.aurora-framework.org/application");
+						modelQueryTagNum++;
+						return true;
 					}
 				}
 			}
 		}
-		
-		return EventModel.HANDLE_NORMAL;
+		return false;
 	}
 
 	public int preCreateSuccessResponse(ServiceContext context)
@@ -79,8 +111,7 @@ public class ModelOutput {
 		CompositeMap parameter = context.getParameter();
 		if (!parameter.getBoolean(KEY_GENERATE_STATE, false))
 			return EventModel.HANDLE_NORMAL;
-		
+
 		return EventModel.HANDLE_STOP;
 	}
 }
-
