8fae6e43a0b6c0c816b537079553d4cf720a6c30
jeschou.zsy <jeschou.zsy@gmail.com>
2012-10-26 06:45:05 +0000

    新建空的工程,会自动新建最基本的目录,并自动设置工程属性 根据已存在工程新建工程,会自动设置工程属性,并触发build

--- uncertain_ide/aurora/ide/project/wizard/ProjectWizard.java
+++ uncertain_ide/aurora/ide/project/wizard/ProjectWizard.java
@@ -1,18 +1,26 @@
 package aurora.ide.project.wizard;
 
+import java.lang.reflect.InvocationTargetException;
 
-import org.eclipse.core.resources.IProjectDescription;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
 
+import aurora.ide.AuroraPlugin;
 import aurora.ide.AuroraProjectNature;
+import aurora.ide.builder.ResourceUtil;
 import aurora.ide.helpers.DialogUtil;
+import aurora.ide.project.propertypage.ProjectPropertyPage;
 
-
-public class ProjectWizard extends BasicNewProjectResourceWizard {
+public class ProjectWizard extends BasicNewProjectResourceWizard implements
+		Runnable, IRunnableWithProgress {
 
 	public static final String copyright = "(c) Copyright HAND Enterprise Solutions Company Ltd.";
 
@@ -23,8 +31,22 @@ public class ProjectWizard extends BasicNewProjectResourceWizard {
 	public boolean performFinish() {
 		if (!super.performFinish())
 			return false;
+		IProject proj = getNewProject();
+		if (proj == null)
+			return true;
+		IFolder web_inf = ResourceUtil.searchWebInf(proj);
+		if (web_inf == null) {
+			createBasicFolders(proj);
+		} else {
+			setPersistentProperty(proj, web_inf.getParent().getFullPath()
+					.toString(), web_inf.getFolder("classes").getFullPath()
+					.toString());
+		}
 		try {
-			AuroraProjectNature.addAuroraNature(getNewProject());
+			AuroraProjectNature.addAuroraNature(proj);
+			if (web_inf != null) {
+				startBuild();
+			}
 		} catch (CoreException e) {
 			DialogUtil.logErrorException(e);
 		}
@@ -32,6 +54,58 @@ public class ProjectWizard extends BasicNewProjectResourceWizard {
 	}
 
 	public void createPageControls(Composite pageContainer) {
-		 super.createPageControls(pageContainer);
+		super.createPageControls(pageContainer);
+	}
+
+	private void createBasicFolders(IProject proj) {
+		String[] segs = { "webRoot", "WEB-INF", "classes" };
+		IPath path = proj.getFullPath();
+		try {
+			for (String s : segs) {
+				path = path.append(s);
+				IFolder f = proj.getParent().getFolder(path);
+				f.create(false, true, null);
+			}
+			setPersistentProperty(proj, proj.getFullPath().append(segs[0])
+					.toString(), path.toString());
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void setPersistentProperty(IProject proj, String webHome,
+			String bmHome) {
+		try {
+			proj.setPersistentProperty(ProjectPropertyPage.WebQN, webHome);
+			proj.setPersistentProperty(ProjectPropertyPage.BMQN, bmHome);
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void startBuild() {
+		Display.getCurrent().asyncExec(this);
+	}
+
+	@Override
+	public void run() {
+		try {
+			AuroraPlugin.getDefault().getWorkbench().getProgressService()
+					.busyCursorWhile(this);
+		} catch (InvocationTargetException e1) {
+			e1.printStackTrace();
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run(IProgressMonitor monitor) throws InvocationTargetException,
+			InterruptedException {
+		try {
+			getNewProject()
+					.build(IncrementalProjectBuilder.FULL_BUILD, monitor);
+		} catch (CoreException e) {
+		}
 	}
 }
