9763e215a58a471c8d3b56ba78e10f0c46abf1d7
rufus.sly@gmail.com <rufus.sly@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-11-13 02:55:32 +0000

    ide:fix db connection

--- trunk/uncertain_ide/aurora/ide/helpers/DBConnectionUtil.java
+++ trunk/uncertain_ide/aurora/ide/helpers/DBConnectionUtil.java
@@ -1,39 +1,168 @@
 package aurora.ide.helpers;
 
+import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.HashMap;
 
 import javax.sql.DataSource;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 
-import uncertain.core.UncertainEngine;
 import uncertain.ocm.IObjectRegistry;
+import aurora.ide.AuroraPlugin;
+import aurora.ide.fake.uncertain.engine.FakeUncertainEngine;
+import aurora.ide.project.AuroraProject;
 
 public class DBConnectionUtil {
 
+	final private static HashMap<IProject, FakeUncertainEngine> project_engine = new HashMap<IProject, FakeUncertainEngine>();
 
-	public static Connection getDBConnection(IProject project) throws ApplicationException {
-		if (project == null)
-			throw new ApplicationException("项目参数不能为空");
-		Connection conn = null;
-		UncertainEngine ue = UncertainEngineUtil.initUncertainProject(project);
-		if (ue == null)
-			throw new ApplicationException("获取UncertainProject失败!");
-		conn = getConnection(ue);
-		return conn;
+	static {
+
+		AuroraPlugin.getWorkspace().addResourceChangeListener(
+				new IResourceChangeListener() {
+					public void resourceChanged(IResourceChangeEvent event) {
+						IResourceDelta delta = event.getDelta();
+						if (delta == null)
+							return;
+						try {
+							delta.accept(new IResourceDeltaVisitor() {
+								public boolean visit(IResourceDelta delta)
+										throws CoreException {
+									IResource resource = delta.getResource();
+									if (resource instanceof IFile) {
+										IFile file = (IFile) resource;
+										if ("datasource.config".equals(file
+												.getName())
+												|| "0.datasource.config"
+														.equals(file.getName())) {
+											IProject project = file
+													.getProject();
+											FakeUncertainEngine fakeUncertainEngine = project_engine
+													.get(project);
+											if (fakeUncertainEngine != null) {
+												fakeUncertainEngine.shutdown();
+											}
+										}
+										return false;
+									}
+									return true;
+								}
+							});
+						} catch (CoreException e) {
+						}
+					}
+				});
 	}
-	public static Connection getConnection(UncertainEngine ue) throws ApplicationException{
-		if(ue == null)
-			return null;
-		Connection conn = null;
-		IObjectRegistry mObjectRegistry = ue.getObjectRegistry();
-		DataSource ds = (DataSource) mObjectRegistry.getInstanceOfType(DataSource.class);
-		try {
-			conn = ds.getConnection();
-		} catch (SQLException e) {
-			throw new ApplicationException("获取数据库连接失败!请查看" + AuroraConstant.DbConfigFileName + "是否配置正确.", e);
+
+	private static FakeUncertainEngine createFakeUncertainEngine(
+			IProject project) throws ApplicationException {
+		AuroraProject ap = new AuroraProject(project);
+		IFolder web_home = ap.getWeb_home();
+		IFolder web_inf = ap.getWeb_inf();
+		if (web_home == null || web_inf == null) {
+			// do sth
+			throw new ApplicationException("Aurora 工程设置不正确 ");
 		}
-		return conn ;
+		FakeUncertainEngine fue = new FakeUncertainEngine(web_home
+				.getRawLocation().toOSString(), web_inf.getRawLocation()
+				.toOSString());
+		return fue;
 	}
+
+	private class Runner implements IRunnableWithProgress {
+
+		private Connection conn;
+		private FakeUncertainEngine fue;
+
+		public Runner(FakeUncertainEngine fue) {
+			super();
+			this.fue = fue;
+		}
+
+		public void run(IProgressMonitor monitor)
+				throws InvocationTargetException, InterruptedException {
+
+			monitor.setTaskName("正在建立数据库连接");
+			if (!fue.isRunning()) {
+				monitor.setTaskName("启动Aurora引擎");
+				fue.startup();
+			}
+			monitor.setTaskName("获取数据库连接");
+			IObjectRegistry mObjectRegistry = fue.getObjectRegistry();
+			DataSource ds = (DataSource) mObjectRegistry
+					.getInstanceOfType(DataSource.class);
+			try {
+				setConn(ds.getConnection());
+			} catch (SQLException e) {
+				throw new InvocationTargetException(e);
+			}
+		}
+
+		public Connection getConn() {
+			return conn;
+		}
+
+		public void setConn(Connection conn) {
+			this.conn = conn;
+		}
+
+	}
+
+	public static Connection getDBConnection(IProject project)
+			throws ApplicationException {
+		FakeUncertainEngine fue = project_engine.get(project);
+		if (fue == null) {
+			fue = createFakeUncertainEngine(project);
+			project_engine.put(project, fue);
+		}
+		try {
+			Runner runnable = new DBConnectionUtil().new Runner(fue);
+			AuroraPlugin.getDefault().getWorkbench().getProgressService()
+					.busyCursorWhile(runnable);
+			return runnable.getConn();
+		} catch (InvocationTargetException e) {
+			throw new ApplicationException("获取数据库连接失败!请查看"
+					+ AuroraConstant.DbConfigFileName + "是否配置正确.", e);
+		} catch (InterruptedException e) {
+			throw new ApplicationException("获取数据库连接失败!请查看"
+					+ AuroraConstant.DbConfigFileName + "是否配置正确.", e);
+		}
+
+	}
+
+	public static boolean testDBConnection(IProject project, String webHome)
+			throws ApplicationException {
+		return getDBConnection(project) != null;
+		// if (webHome == null)
+		// throw new ApplicationException("Web目录不能为空");
+		// UncertainEngine ue =
+		// UncertainEngineUtil.initUncertainProject(webHome);
+		// if (ue == null)
+		// throw new ApplicationException("获取UncertainProject失败!");
+		// IObjectRegistry mObjectRegistry = ue.getObjectRegistry();
+		// DataSource ds = (DataSource) mObjectRegistry
+		// .getInstanceOfType(DataSource.class);
+		// try {
+		// Connection conn = ds.getConnection();
+		// conn.close();
+		// } catch (SQLException e) {
+		// e.printStackTrace();
+		// throw new ApplicationException("获取数据库连接失败!请查看"
+		// + AuroraConstant.DbConfigFileName + "是否配置正确.", e);
+		// }
+		// return true;
+	}
+
 }
--- trunk/uncertain_ide/aurora/ide/helpers/DBEngineInitiator.java
+++ trunk/uncertain_ide/aurora/ide/helpers/DBEngineInitiator.java
@@ -10,51 +10,51 @@ import aurora.database.service.DatabaseServiceFactory;
 import aurora.ide.AuroraPlugin;
 
 public class DBEngineInitiator {
-
-	File mHomeDir;
-	File mConfigPath;
-	UncertainEngine uncertainEngine;
-
-	public DBEngineInitiator(File homeDir, File configPath) {
-		mHomeDir = homeDir;
-		mConfigPath = configPath;
-	}
-
-	public void init() throws Exception {
-		uncertainEngine = UncertainEngineUtil.getUncertainEngine(mConfigPath);
-		DirectoryConfig dirConfig = uncertainEngine.getDirectoryConfig();
-		dirConfig.setBaseDirectory(mHomeDir.getCanonicalPath());
-		uncertainEngine.setContinueLoadConfigWithException(true);
-		try {
-			uncertainEngine.startup();
-		} catch (Throwable e) {
-			throw new ApplicationException("启用UncertainEngine失败!", e);
-		}
-		DataSource ds = (DataSource) (uncertainEngine.getObjectRegistry().getInstanceOfType(DataSource.class));
-		if (ds == null) {
-			throw new ApplicationException(
-					"startup UncertainEngine failed: Can not get DataSource instance. Please check database config files.");
-		}
-		DatabaseServiceFactory dbs = (DatabaseServiceFactory) (uncertainEngine.getObjectRegistry()
-				.getInstanceOfType(DatabaseServiceFactory.class));
-		if (dbs == null) {
-			throw new ApplicationException(
-					"startup UncertainEngine failed: Can not get DatabaseServiceFactory instance. Please check database config files.");
-		}
-	}
-
-	/**
-	 * @return the mHomeDir
-	 */
-	public File getHomeDir() {
-		return mHomeDir;
-	}
-
-	/**
-	 * @return the uncertainEngine
-	 */
-	public UncertainEngine getUncertainEngine() {
-		return uncertainEngine;
-	}
+//
+//	File mHomeDir;
+//	File mConfigPath;
+//	UncertainEngine uncertainEngine;
+//
+//	public DBEngineInitiator(File homeDir, File configPath) {
+//		mHomeDir = homeDir;
+//		mConfigPath = configPath;
+//	}
+//
+//	public void init() throws Exception {
+//		uncertainEngine = UncertainEngineUtil.getUncertainEngine(mConfigPath);
+//		DirectoryConfig dirConfig = uncertainEngine.getDirectoryConfig();
+//		dirConfig.setBaseDirectory(mHomeDir.getCanonicalPath());
+//		uncertainEngine.setContinueLoadConfigWithException(true);
+//		try {
+//			uncertainEngine.startup();
+//		} catch (Throwable e) {
+//			throw new ApplicationException("启用UncertainEngine失败!", e);
+//		}
+//		DataSource ds = (DataSource) (uncertainEngine.getObjectRegistry().getInstanceOfType(DataSource.class));
+//		if (ds == null) {
+//			throw new ApplicationException(
+//					"startup UncertainEngine failed: Can not get DataSource instance. Please check database config files.");
+//		}
+//		DatabaseServiceFactory dbs = (DatabaseServiceFactory) (uncertainEngine.getObjectRegistry()
+//				.getInstanceOfType(DatabaseServiceFactory.class));
+//		if (dbs == null) {
+//			throw new ApplicationException(
+//					"startup UncertainEngine failed: Can not get DatabaseServiceFactory instance. Please check database config files.");
+//		}
+//	}
+//
+//	/**
+//	 * @return the mHomeDir
+//	 */
+//	public File getHomeDir() {
+//		return mHomeDir;
+//	}
+//
+//	/**
+//	 * @return the uncertainEngine
+//	 */
+//	public UncertainEngine getUncertainEngine() {
+//		return uncertainEngine;
+//	}
 
 }
