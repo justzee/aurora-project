91636d5b65f026a7651100dfd96b8211589bc5ad
jeschou.zsy <jeschou.zsy@gmail.com>
2012-11-23 02:56:17 +0000

    修正bug：选择本地字段时，key是一个对象

--- uncertain_ide/aurora/ide/celleditor/LocalFieldReferenceCellEditor.java
+++ uncertain_ide/aurora/ide/celleditor/LocalFieldReferenceCellEditor.java
@@ -1,6 +1,5 @@
 package aurora.ide.celleditor;
 
-
 import java.util.Iterator;
 
 import org.eclipse.jface.viewers.TableViewer;
@@ -26,13 +25,12 @@ public class LocalFieldReferenceCellEditor extends StringTextCellEditor {
 		super(cellProperties);
 	}
 
-
 	protected void addCellListener() {
 		getCellControl().addMouseListener(new MouseListener() {
-			
-			public void mouseUp(MouseEvent e) {		
+
+			public void mouseUp(MouseEvent e) {
 			}
-			
+
 			public void mouseDown(MouseEvent e) {
 				try {
 					fireEvent();
@@ -40,73 +38,82 @@ public class LocalFieldReferenceCellEditor extends StringTextCellEditor {
 					DialogUtil.showExceptionMessageBox(ex);
 				}
 			}
-			
+
 			public void mouseDoubleClick(MouseEvent e) {
 			}
 		});
 		getCellControl().addFocusListener(new FocusListener() {
 			public void focusLost(FocusEvent e) {
-				if(isTableItemEditor())
-					rebuildCellEditor(cellProperties.getTableItem());				
+				if (isTableItemEditor())
+					rebuildCellEditor(cellProperties.getTableItem());
 			}
+
 			public void focusGained(FocusEvent e) {
-				
+
 			}
 		});
 	}
-	private void fireEvent() throws ApplicationException{
-		CompositeMap root= getData();
-		if(root == null)
-			return ;
-		CompositeMap fields = isTableItemEditor()?root.getChild("fields"):root.getParent().getChild("fields");
+
+	private void fireEvent() throws ApplicationException {
+		CompositeMap root = getData();
+		if (root == null)
+			return;
+		CompositeMap fields = isTableItemEditor() ? root.getChild("fields")
+				: root.getParent().getChild("fields");
 		CompositeMap filedNames = new CommentCompositeMap();
-		if(fields != null)
-			for(Iterator it = fields.getChildsNotNull().iterator();it.hasNext();){
-				CompositeMap child = (CompositeMap)it.next();
+		if (fields != null)
+			for (Iterator it = fields.getChildsNotNull().iterator(); it
+					.hasNext();) {
+				CompositeMap child = (CompositeMap) it.next();
 				String targetNode = child.getString("name");
-				if(targetNode == null)
+				if (targetNode == null)
 					continue;
 				CompositeMap newChild = new CommentCompositeMap();
 				newChild.put("name", targetNode);
 				filedNames.addChild(newChild);
-	
+
 			}
-		String[] columnProperties = {"name"};
-		GridViewer grid = new GridViewer(IGridViewer.filterBar|IGridViewer.NoToolBar);
+		String[] columnProperties = { "name" };
+		GridViewer grid = new GridViewer(IGridViewer.filterBar
+				| IGridViewer.NoToolBar);
 		grid.setData(filedNames);
 		grid.setFilterColumn("name");
 		grid.setColumnNames(columnProperties);
-		GridDialog dialog = new GridDialog(new Shell(),grid);
-		if (dialog.open() == Window.OK&&dialog.getSelected() != null) {
+		GridDialog dialog = new GridDialog(new Shell(), grid);
+		if (dialog.open() == Window.OK && dialog.getSelected() != null) {
 			String value = dialog.getSelected().getString("name");
 			setValue(value);
-			if(isTableItemEditor()){
-				cellProperties.getRecord().put(cellProperties.getColumnName(), value);
-			}else{
-				TableItem item =cellProperties.getTableViewer().getViewer().getTable().getSelection()[0];
-				CompositeMap data = (CompositeMap)item.getData();
-				data.put(cellProperties, value);
-//				Object oldValue = data.get(property);
-				
+			if (isTableItemEditor()) {
+				cellProperties.getRecord().put(cellProperties.getColumnName(),
+						value);
+			} else {
+				TableItem item = cellProperties.getTableViewer().getViewer()
+						.getTable().getSelection()[0];
+				CompositeMap data = (CompositeMap) item.getData();
+				data.put(cellProperties.getColumnName(), value);
+				// Object oldValue = data.get(property);
+
 			}
 			cellProperties.getTableViewer().refresh(true);
 		}
 	}
-	private CompositeMap getData(){
-		if(cellProperties.getTableItem() != null){
+
+	private CompositeMap getData() {
+		if (cellProperties.getTableItem() != null) {
 			return cellProperties.getRecord().getRoot();
 		}
 		TableViewer tableViewer = cellProperties.getTableViewer().getViewer();
-		if(tableViewer == null)
+		if (tableViewer == null)
 			return null;
 		Object dataObject = tableViewer.getInput();
-		if( dataObject == null)
+		if (dataObject == null)
 			return null;
-		return (CompositeMap)dataObject;
-		
+		return (CompositeMap) dataObject;
+
 	}
-	private boolean isTableItemEditor(){
+
+	private boolean isTableItemEditor() {
 		return cellProperties.getTableItem() != null;
 	}
-	
+
 }
