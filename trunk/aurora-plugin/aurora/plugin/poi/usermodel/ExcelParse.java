164bbe9b627cac1308947dc981fdc66e5a9c2fee
bobbie.zou@gmail.com <bobbie.zou@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2013-11-19 09:49:37 +0000

    修改了date类型的判断方式，支持公式

--- trunk/aurora-plugin/aurora/plugin/poi/usermodel/ExcelParse.java
+++ trunk/aurora-plugin/aurora/plugin/poi/usermodel/ExcelParse.java
@@ -11,6 +11,9 @@ import java.util.List;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellValue;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -22,6 +25,7 @@ import aurora.plugin.dataimport.ImportExcel;
 
 public class ExcelParse {
 	DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+	FormulaEvaluator evaluator;
 
 	public void parseFile(InputStream is, ImportExcel importProcessor,
 			String suffix) throws IOException, SQLException {
@@ -31,6 +35,7 @@ public class ExcelParse {
 		} else if (ImportExcel.XLSX_KEY.equalsIgnoreCase(suffix)) {
 			wb = new XSSFWorkbook(is);
 		}
+		evaluator = wb.getCreationHelper().createFormulaEvaluator();
 		Sheet sheet = null;
 		for (int i = 0, l = wb.getNumberOfSheets(); i < l; i++) {
 			sheet = wb.getSheetAt(i);
@@ -43,68 +48,86 @@ public class ExcelParse {
 		Row row;
 		Cell cell;
 		CompositeMap record;
-		List<String> cellList=new LinkedList<String>();
+		List<String> cellList = new LinkedList<String>();
 		String sheetName = sheet.getSheetName();
-		int  l = sheet.getLastRowNum();
-		if(sheet.getRow(0)==null)return;
-		int maxCellNum=sheet.getRow(0).getLastCellNum();
-		System.out.println("导入文件sheet("+sheetName+")最后一行是："+(l+1));
+		int l = sheet.getLastRowNum();
+		if (sheet.getRow(0) == null)
+			return;
+		int maxCellNum = sheet.getRow(0).getLastCellNum();
+		System.out.println("导入文件sheet(" + sheetName + ")最后一行是：" + (l + 1));
 		boolean is_write = false;
-		boolean is_new=true;		
+		boolean is_new = true;
 		for (int i = 0; i <= l; i++) {
 			row = sheet.getRow(i);
 			if (row == null)
 				break;
 			record = new CompositeMap("record");
 			record.putBoolean("is_new", is_new);
-			is_new=false;
-			is_write = false;			
-			
-			if(i==0){
-				record.putString("sheetName", "sheetName");	
-			}
-			else
-				record.putString("sheetName", sheetName);			
+			is_new = false;
+			is_write = false;
+
+			if (i == 0) {
+				record.putString("sheetName", "sheetName");
+			} else
+				record.putString("sheetName", sheetName);
 			for (int j = 0; j < maxCellNum; j++) {
 				String value = null;
 				cell = row.getCell(j);
 				if (cell != null) {
-					if (cell.getCellType() == Cell.CELL_TYPE_STRING)
-						value = cell.getRichStringCellValue().toString();
-					if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
-						try {
-							Double.parseDouble(cell.toString());
-							value=BigDecimal.valueOf(cell.getNumericCellValue()).toPlainString();							
-						} catch (Exception e) {
-							if (cell.getDateCellValue() != null)
-								value = df.format(cell.getDateCellValue());
+					CellValue cellValue = evaluator.evaluate(cell);
+					if (cellValue != null) {
+						switch (cellValue.getCellType()) {
+						case Cell.CELL_TYPE_BOOLEAN:
+							value = Boolean.toString(cellValue
+									.getBooleanValue());
+							break;
+						case Cell.CELL_TYPE_NUMERIC:
+							if (DateUtil.isCellDateFormatted(cell)) {
+								if (cell.getDateCellValue() != null)
+									value = df.format(cell.getDateCellValue());
+								System.out.println(value);
+							} else {
+								value = BigDecimal
+										.valueOf(cellValue.getNumberValue())
+										.stripTrailingZeros().toPlainString();
+							}
+							break;
+						case Cell.CELL_TYPE_STRING:
+							value = cellValue.getStringValue();
+							break;
+						case Cell.CELL_TYPE_BLANK:
+							break;
+						case Cell.CELL_TYPE_ERROR:
+							break;
+
+						// CELL_TYPE_FORMULA will never happen
+						case Cell.CELL_TYPE_FORMULA:
+							break;
 						}
 					}
-					if (cell.getCellType() == Cell.CELL_TYPE_BOOLEAN)
-						value = Boolean.toString(cell.getBooleanCellValue());
 					if (value != null && !"".equalsIgnoreCase(value)) {
 						is_write = true;
 						record.putString("C" + j, value);
 						cellList.add(value);
-					}else{
+					} else {
 						cellList.add("");
 					}
-				}else{
+				} else {
 					cellList.add("");
 				}
 			}
-			if(i==0){
-				int indexcount=0;
-				for(int index=cellList.size();index>0;index--){
-					String value=cellList.get(index-1);
-					if("".equals(value)){
+			if (i == 0) {
+				int indexcount = 0;
+				for (int index = cellList.size(); index > 0; index--) {
+					String value = cellList.get(index - 1);
+					if ("".equals(value)) {
 						indexcount++;
 						continue;
-					}else{
+					} else {
 						break;
 					}
 				}
-				maxCellNum=maxCellNum-indexcount;
+				maxCellNum = maxCellNum - indexcount;
 			}
 			record.putInt("maxCell", maxCellNum);
 			if (is_write)
