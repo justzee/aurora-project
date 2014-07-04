17d8fbe2fd82d67b7c5ce7d6a0aa300d177603ef
linjinxiao@gmail.com <linjinxiao@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2014-07-04 06:37:05 +0000

    附件功能关联到sharepoint

---
+++ trunk/aurora-plugin/com/microsoft/schemas/sharepoint/ObjectFactory.java
@@ -0,0 +1,120 @@
+
+package com.microsoft.schemas.sharepoint;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the com.microsoft.sharepoint.webservices package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.microsoft.sharepoint.webservices
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link DestinationUrlCollection }
+     * 
+     */
+    public DestinationUrlCollection createDestinationUrlCollection() {
+        return new DestinationUrlCollection();
+    }
+
+    /**
+     * Create an instance of {@link FieldInformation }
+     * 
+     */
+    public FieldInformation createFieldInformation() {
+        return new FieldInformation();
+    }
+
+    /**
+     * Create an instance of {@link GetItemResponse }
+     * 
+     */
+    public GetItemResponse createGetItemResponse() {
+        return new GetItemResponse();
+    }
+
+    /**
+     * Create an instance of {@link FieldInformationCollection }
+     * 
+     */
+    public FieldInformationCollection createFieldInformationCollection() {
+        return new FieldInformationCollection();
+    }
+
+    /**
+     * Create an instance of {@link CopyResultCollection }
+     * 
+     */
+    public CopyResultCollection createCopyResultCollection() {
+        return new CopyResultCollection();
+    }
+
+    /**
+     * Create an instance of {@link CopyIntoItemsLocal }
+     * 
+     */
+    public CopyIntoItemsLocal createCopyIntoItemsLocal() {
+        return new CopyIntoItemsLocal();
+    }
+
+    /**
+     * Create an instance of {@link CopyIntoItemsLocalResponse }
+     * 
+     */
+    public CopyIntoItemsLocalResponse createCopyIntoItemsLocalResponse() {
+        return new CopyIntoItemsLocalResponse();
+    }
+
+    /**
+     * Create an instance of {@link CopyResult }
+     * 
+     */
+    public CopyResult createCopyResult() {
+        return new CopyResult();
+    }
+
+    /**
+     * Create an instance of {@link GetItem }
+     * 
+     */
+    public GetItem createGetItem() {
+        return new GetItem();
+    }
+
+    /**
+     * Create an instance of {@link CopyIntoItems }
+     * 
+     */
+    public CopyIntoItems createCopyIntoItems() {
+        return new CopyIntoItems();
+    }
+
+    /**
+     * Create an instance of {@link CopyIntoItemsResponse }
+     * 
+     */
+    public CopyIntoItemsResponse createCopyIntoItemsResponse() {
+        return new CopyIntoItemsResponse();
+    }
+
+}
